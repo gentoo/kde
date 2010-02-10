eb41c28d16ccd5b02daeff9fdab9e231bdba4067
Dennis Schridde <devurandom@gmx.net>
2010-02-10 22:37:01 +0100

    [Documentation/metadata-mirror-update.sh] The metadata generation script I use to keep my mirror up-to-date

---
+++ Documentation/metadata-mirror-update.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+METADATA_MIRROR_DIR=/path/to/your/mirror/dir
+
+# METADATA_MIRROR_DIR must contain:
+#  * a folder $overlay/ for each overlay you intent to mirror.
+#  * a folder cache/ to store the cache of all overlays.
+#    (so it is not mixed with the cache of your system portage-trees)
+# METADATA_MIRROR_DIR/$overlay/ must contain:
+#  * a file etc/make.conf with following contents:
+#    PORTDIR=/your/portage/directory
+#    PORTDIR_OVERLAY=/path/to/your/mirror/dir/$overlay/repo
+#    FEATURES="${FEATURES} userpriv userfetch usersandbox usersync metadata-transfer"
+#  * a directory repo/ containing the checked out repository you want to create metadata from.
+# For speed reasons it is advisable to have a file etc/portage/modules containing:
+#    portdbapi.auxdbmodule = portage.cache.sqlite.database
+
+die() {
+        echo "USAGE: $0 <overlay>" 1>&2
+        echo "ERROR: $@" 1>&2
+        exit -1
+}
+
+[[ "$1" ]] || die 'overlay'
+overlay="$1" ; shift
+
+case $overlay in
+        kde) update="git --git-dir=$METADATA_MIRROR_DIR/kde/repo/.git/ pull" ;;
+esac
+
+if $update ; then
+        exec egencache --config-root=$METADATA_MIRROR_DIR/$overlay/ --cache-dir=$METADATA_MIRROR_DIR/cache/ --repo=$overlay --update
+fi
