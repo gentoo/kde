71dab7a41d0c7b195aa3dccaae272bb7093b1fb7
Alexey Shvetsov <alexxy@gentoo.org>
2010-05-01 13:24:24 +0400

    [Documentation/maintainers] Update kde-l10n repack script

--- Documentation/maintainers/kde-l10n-repack.sh
+++ Documentation/maintainers/kde-l10n-repack.sh
@@ -7,27 +7,19 @@ LANGS="af ar be bg bn bn_IN br ca cs csb cy da de el en_GB eo es et eu fa fi fr
 	ms mt nb nds ne nl nn nso oc pa pl pt pt_BR ro ru rw se sk sl sr sv ta te tg
 	th tr uk uz vi wa xh zh_CN zh_HK zh_TW"
 
-for lng in ${LANGS}
-do
-svn up -r ${SVNREV} kde-l10n-${lng}
-done
-
-svn up -r ${SVNREV} scripts
-
-for lng in ${LANGS}
-do
-	cp -r kde-l10n-${lng} kde-l10n-${lng}-${KDEVER}
-	cp -r scripts kde-l10n-${lng}-${KDEVER}/
-	rm -rf kde-l10n-${lng}-${KDEVER}/{docmessages,messages,docs}/extragear*
-	rm -rf kde-l10n-${lng}-${KDEVER}/{docmessages,messages,docs}/playground*
-	rm -rf kde-l10n-${lng}-${KDEVER}/{docmessages,messages,docs}/kdereview
-	rm -rf kde-l10n-${lng}-${KDEVER}/{docmessages,messages,docs}/koffice
-	rm -rf kde-l10n-${lng}-${KDEVER}/{docmessages,messages,docs}/kdevelop
-	rm -rf kde-l10n-${lng}-${KDEVER}/{docmessages,messages,docs}/qt
-	cd kde-l10n-${lng}-${KDEVER}
+# Update repo
+svn up -r ${SVNREV} kde-l10n
+# copy repo
+cp -r kde-l10n kde-l10n_packages
+cd kde-l10n_packages
+for ${LNG} in ${LANGS}; do
+	rm -rf ./${LNG}/messages/{extragear-*,koffice,playground-*,qt,kdevelop,kdereview}
+	./scripts/autogen.sh ${LNG}
+	mv ${LNG} kde-l10n-${LNG}-${KDEVER}
+	cd kde-l10n-${LNG}-${KDEVER}
 	find . -xtype d -iname .svn | xargs rm -rf
 	cd ../
-	tar cpf kde-l10n-${lng}-${KDEVER}.tar kde-l10n-${lng}-${KDEVER}
-	lzma -9 kde-l10n-${lng}-${KDEVER}.tar
-	rm -rf kde-l10n-${lng}-${KDEVER}
+	tar cpf kde-l10n-${LNG}-${KDEVER}.tar kde-l10n-${LNG}-${KDEVER}
+	xz -9 -v kde-l10n-${LNG}-${KDEVER}.tar
+	rm -rf kde-l10n-${LNG}-${KDEVER}
 done
