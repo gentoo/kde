gentoo/kde

commit fb50ba57024a9c4de2ecee407a1d5b85728e10d4

[kde-base/kdm] Install faces in correct location

kde-base/kdm/Manifest
@@ -4,8 +4,8 @@ AUX kdm-4.3.5-xdm-restart.patch 1593 RMD160 f6bf916ec09049ba7c7a3c65ec86b59302b8
 DIST kdebase-workspace-4.4.85.tar.bz2 64022189 RMD160 aca67d1f946e14ebe38e7a51aaea65c2aff28f71 SHA1 68501d3d7ee209c8e38b7dddc0f836c567fb3584 SHA256 ac9337c6bf2212010358bd2231a893a41f20c90cf3ab27456b87edbb1ea90f47
 DIST kdebase-workspace-4.4.90.tar.bz2 65633682 RMD160 27d62a6d8cb847610aad896a2234e4a901c120e7 SHA1 04aff37ae1be92c64e130e211dba84f8fa40eb95 SHA256 2856a4c69c18018496df3fe843f554d3332aa9f37ae25b76a80faf465baacab8
 EBUILD kdm-4.4.85.ebuild 3304 RMD160 5fafc26d70a48e8044643a4981b7791adb3c6bcc SHA1 2a126b344fb035925dde8bd1dcc902fde0bac16b SHA256 abdadff43bcd01c3f08c23a2477f9bb92bc3fbcd80ecb63319c67dd5efe4ca36
-EBUILD kdm-4.4.90.ebuild 3304 RMD160 5fafc26d70a48e8044643a4981b7791adb3c6bcc SHA1 2a126b344fb035925dde8bd1dcc902fde0bac16b SHA256 abdadff43bcd01c3f08c23a2477f9bb92bc3fbcd80ecb63319c67dd5efe4ca36
-EBUILD kdm-4.4.9999.ebuild 3106 RMD160 596970ced4eaca76251c644368e217171bbd9a36 SHA1 2c8a603d319bcee14d61aa32e27d6fb9880f9e88 SHA256 e457d3f95ef99b8dcef2eedd5ebec8e1d621f9a34f4f98a7485101c486209f9f
-EBUILD kdm-4.5.9999.ebuild 3256 RMD160 d41c0f35fcd5bcf20f2ce4f15aeda63e4230e782 SHA1 089a3829086d7a7208a937fb22f7ed7376e0b7fa SHA256 0e8f743927d817eb31190ca0fe623742f8bfc4ff986b412a900a04dc19dd4193
-EBUILD kdm-9999.ebuild 3256 RMD160 d41c0f35fcd5bcf20f2ce4f15aeda63e4230e782 SHA1 089a3829086d7a7208a937fb22f7ed7376e0b7fa SHA256 0e8f743927d817eb31190ca0fe623742f8bfc4ff986b412a900a04dc19dd4193
+EBUILD kdm-4.4.90.ebuild 3237 RMD160 3e3d13ec511ffdd327208319b2b7b66879f7fa4c SHA1 7f36544ac01ae8855b430bc7bac226a9f841cf2e SHA256 cd636a01544ea26eba012f37c0aac07751f7a34432b75a62d984ee16c3929fc9
+EBUILD kdm-4.4.9999.ebuild 3039 RMD160 7702134f8a80a96fb7d1ed81a323bf056fc0038b SHA1 43445121167d366a8ec7d420bb85bd81aba14a32 SHA256 fa63f6b7ce9c2ab777126c7808566a3866f53db7baecf2c5f3e7e6e4620e6853
+EBUILD kdm-4.5.9999.ebuild 3189 RMD160 91712141c20084d3dea33d79731023a82649fe7a SHA1 7cbb8b58c9bc7d0253d210683b31b643cbb31c11 SHA256 d966432d26585aaf062bfaa32784d703fed0cce8ad840e8bbe7e9e08dbf09074
+EBUILD kdm-9999.ebuild 3189 RMD160 91712141c20084d3dea33d79731023a82649fe7a SHA1 7cbb8b58c9bc7d0253d210683b31b643cbb31c11 SHA256 d966432d26585aaf062bfaa32784d703fed0cce8ad840e8bbe7e9e08dbf09074
 MISC metadata.xml 253 RMD160 dbe3d0cc1778dad287767c5dee6b46b42ccf20ec SHA1 b4723a2baaec997efee2cdc543248a2aad8f1a7e SHA256 955ffc185b3b01315be94542ea43ae1180d5f61ea539a317c45c5746b463eeba

kde-base/kdm/kdm-4.4.90.ebuild
@@ -100,15 +100,15 @@ pkg_postinst() {
 
 	# Set the default kdm face icon if it's not already set by the system admin
 	# because this is user-overrideable in that way, it's not in src_install
-	if [[ ! -e "${EROOT}${KDEDIR}/share/apps/kdm/faces/.default.face.icon" ]]; then
-		mkdir -p "${EROOT}${KDEDIR}/share/apps/kdm/faces"
+	if [[ ! -e "${EPREFIX}${KDM_HOME}/faces/.default.face.icon" ]]; then
+		mkdir -p "${EPREFIX}${KDM_HOME}/faces"
 		cp "${EROOT}${KDEDIR}/share/apps/kdm/pics/users/default1.png" \
-			"${EROOT}${KDEDIR}/share/apps/kdm/faces/.default.face.icon"
+			"${PREFIX}${KDM_HOME}/faces/.default.face.icon"
 	fi
-	if [[ ! -e "${EROOT}${KDEDIR}/share/apps/kdm/faces/root.face.icon" ]]; then
-		mkdir -p "${EROOT}${KDEDIR}/share/apps/kdm/faces"
+	if [[ ! -e "${EPREFIX}${KDM_HOME}/faces/root.face.icon" ]]; then
+		mkdir -p "${EPREFIX}${KDM_HOME}/faces"
 		cp "${EROOT}${KDEDIR}/share/apps/kdm/pics/users/root1.png" \
-			"${EROOT}${KDEDIR}/share/apps/kdm/faces/root.face.icon"
+			"${EPREFIX}${KDM_HOME}/faces/root.face.icon"
 	fi
 
 	if use consolekit; then

kde-base/kdm/kdm-4.4.9999.ebuild
@@ -95,15 +95,15 @@ pkg_postinst() {
 
 	# Set the default kdm face icon if it's not already set by the system admin
 	# because this is user-overrideable in that way, it's not in src_install
-	if [[ ! -e "${EROOT}${KDEDIR}/share/apps/kdm/faces/.default.face.icon" ]]; then
-		mkdir -p "${EROOT}${KDEDIR}/share/apps/kdm/faces"
+	if [[ ! -e "${EPREFIX}${KDM_HOME}/faces/.default.face.icon" ]]; then
+		mkdir -p "${EPREFIX}${KDM_HOME}/faces"
 		cp "${EROOT}${KDEDIR}/share/apps/kdm/pics/users/default1.png" \
-			"${EROOT}${KDEDIR}/share/apps/kdm/faces/.default.face.icon"
+			"${PREFIX}${KDM_HOME}/faces/.default.face.icon"
 	fi
-	if [[ ! -e "${EROOT}${KDEDIR}/share/apps/kdm/faces/root.face.icon" ]]; then
-		mkdir -p "${EROOT}${KDEDIR}/share/apps/kdm/faces"
+	if [[ ! -e "${EPREFIX}${KDM_HOME}/faces/root.face.icon" ]]; then
+		mkdir -p "${EPREFIX}${KDM_HOME}/faces"
 		cp "${EROOT}${KDEDIR}/share/apps/kdm/pics/users/root1.png" \
-			"${EROOT}${KDEDIR}/share/apps/kdm/faces/root.face.icon"
+			"${EPREFIX}${KDM_HOME}/faces/root.face.icon"
 	fi
 
 	if use consolekit; then

kde-base/kdm/kdm-4.5.9999.ebuild
@@ -100,15 +100,15 @@ pkg_postinst() {
 
 	# Set the default kdm face icon if it's not already set by the system admin
 	# because this is user-overrideable in that way, it's not in src_install
-	if [[ ! -e "${EROOT}${KDEDIR}/share/apps/kdm/faces/.default.face.icon" ]]; then
-		mkdir -p "${EROOT}${KDEDIR}/share/apps/kdm/faces"
+	if [[ ! -e "${EPREFIX}${KDM_HOME}/faces/.default.face.icon" ]]; then
+		mkdir -p "${EPREFIX}${KDM_HOME}/faces"
 		cp "${EROOT}${KDEDIR}/share/apps/kdm/pics/users/default1.png" \
-			"${EROOT}${KDEDIR}/share/apps/kdm/faces/.default.face.icon"
+			"${PREFIX}${KDM_HOME}/faces/.default.face.icon"
 	fi
-	if [[ ! -e "${EROOT}${KDEDIR}/share/apps/kdm/faces/root.face.icon" ]]; then
-		mkdir -p "${EROOT}${KDEDIR}/share/apps/kdm/faces"
+	if [[ ! -e "${EPREFIX}${KDM_HOME}/faces/root.face.icon" ]]; then
+		mkdir -p "${EPREFIX}${KDM_HOME}/faces"
 		cp "${EROOT}${KDEDIR}/share/apps/kdm/pics/users/root1.png" \
-			"${EROOT}${KDEDIR}/share/apps/kdm/faces/root.face.icon"
+			"${EPREFIX}${KDM_HOME}/faces/root.face.icon"
 	fi
 
 	if use consolekit; then

kde-base/kdm/kdm-9999.ebuild
@@ -100,15 +100,15 @@ pkg_postinst() {
 
 	# Set the default kdm face icon if it's not already set by the system admin
 	# because this is user-overrideable in that way, it's not in src_install
-	if [[ ! -e "${EROOT}${KDEDIR}/share/apps/kdm/faces/.default.face.icon" ]]; then
-		mkdir -p "${EROOT}${KDEDIR}/share/apps/kdm/faces"
+	if [[ ! -e "${EPREFIX}${KDM_HOME}/faces/.default.face.icon" ]]; then
+		mkdir -p "${EPREFIX}${KDM_HOME}/faces"
 		cp "${EROOT}${KDEDIR}/share/apps/kdm/pics/users/default1.png" \
-			"${EROOT}${KDEDIR}/share/apps/kdm/faces/.default.face.icon"
+			"${PREFIX}${KDM_HOME}/faces/.default.face.icon"
 	fi
-	if [[ ! -e "${EROOT}${KDEDIR}/share/apps/kdm/faces/root.face.icon" ]]; then
-		mkdir -p "${EROOT}${KDEDIR}/share/apps/kdm/faces"
+	if [[ ! -e "${EPREFIX}${KDM_HOME}/faces/root.face.icon" ]]; then
+		mkdir -p "${EPREFIX}${KDM_HOME}/faces"
 		cp "${EROOT}${KDEDIR}/share/apps/kdm/pics/users/root1.png" \
-			"${EROOT}${KDEDIR}/share/apps/kdm/faces/root.face.icon"
+			"${EPREFIX}${KDM_HOME}/faces/root.face.icon"
 	fi
 
 	if use consolekit; then

commit 4c1283e3ba4bb9e5fc4752f8c0a86bdc3201b5e3

[kde-base/kdm] Fix typo in face images location introduced in fb50ba57024a9c4

kde-base/kdm/Manifest
@@ -3,9 +3,9 @@ AUX kdm-4-gentoo-xinitrc.d.patch 793 RMD160 1e22f23d7cbd4764b4a7675968e0a387de2f
 AUX kdm-4.3.5-xdm-restart.patch 1593 RMD160 f6bf916ec09049ba7c7a3c65ec86b59302b8f3be SHA1 f70ae3f876f3840364c698d33d60b15395a457c4 SHA256 0f2f2d9dfba1c0a8a82c6b92cb55ff5afe77fc8b5cbe280fb7fbba19f8327866
 DIST kdebase-workspace-4.4.85.tar.bz2 64022189 RMD160 aca67d1f946e14ebe38e7a51aaea65c2aff28f71 SHA1 68501d3d7ee209c8e38b7dddc0f836c567fb3584 SHA256 ac9337c6bf2212010358bd2231a893a41f20c90cf3ab27456b87edbb1ea90f47
 DIST kdebase-workspace-4.4.90.tar.bz2 65633682 RMD160 27d62a6d8cb847610aad896a2234e4a901c120e7 SHA1 04aff37ae1be92c64e130e211dba84f8fa40eb95 SHA256 2856a4c69c18018496df3fe843f554d3332aa9f37ae25b76a80faf465baacab8
-EBUILD kdm-4.4.85.ebuild 3304 RMD160 5fafc26d70a48e8044643a4981b7791adb3c6bcc SHA1 2a126b344fb035925dde8bd1dcc902fde0bac16b SHA256 abdadff43bcd01c3f08c23a2477f9bb92bc3fbcd80ecb63319c67dd5efe4ca36
-EBUILD kdm-4.4.90.ebuild 3237 RMD160 3e3d13ec511ffdd327208319b2b7b66879f7fa4c SHA1 7f36544ac01ae8855b430bc7bac226a9f841cf2e SHA256 cd636a01544ea26eba012f37c0aac07751f7a34432b75a62d984ee16c3929fc9
-EBUILD kdm-4.4.9999.ebuild 3039 RMD160 7702134f8a80a96fb7d1ed81a323bf056fc0038b SHA1 43445121167d366a8ec7d420bb85bd81aba14a32 SHA256 fa63f6b7ce9c2ab777126c7808566a3866f53db7baecf2c5f3e7e6e4620e6853
-EBUILD kdm-4.5.9999.ebuild 3144 RMD160 fb3bc6cb631de6d8496e9f4f7ba6babea813d0d8 SHA1 2f77449c54a417fe04dbd09b0d10be0fd5577255 SHA256 7f5bef535ba2a3eb27958641e97ce716934767e07d0f059e531685bbade2e32b
-EBUILD kdm-9999.ebuild 3144 RMD160 fb3bc6cb631de6d8496e9f4f7ba6babea813d0d8 SHA1 2f77449c54a417fe04dbd09b0d10be0fd5577255 SHA256 7f5bef535ba2a3eb27958641e97ce716934767e07d0f059e531685bbade2e32b
+EBUILD kdm-4.4.85.ebuild 3238 RMD160 74397f3a1b13673d1e098c0d74611a32b1374c98 SHA1 e716b1912696eda5c6b6ce40d95523725e6d053f SHA256 463ca322e282f5c4574d9882845e8b371fdaececc681d161fc706bc4f3f16b70
+EBUILD kdm-4.4.90.ebuild 3238 RMD160 74397f3a1b13673d1e098c0d74611a32b1374c98 SHA1 e716b1912696eda5c6b6ce40d95523725e6d053f SHA256 463ca322e282f5c4574d9882845e8b371fdaececc681d161fc706bc4f3f16b70
+EBUILD kdm-4.4.9999.ebuild 3040 RMD160 fae479842189d2a8641380aa1dc2ad8475b8207e SHA1 efdce92ca7db760d49ceb62997e690e98f581895 SHA256 2539de80be139568e1c63e40f9fd3437e7854e8ff087f9f58dd6e991287531b1
+EBUILD kdm-4.5.9999.ebuild 3145 RMD160 5cd8ebd48792fd300204d62f19f0f4b26d4405ac SHA1 959d2b4d12979bc824cf2c08edf4aec7cdbed6df SHA256 e091478f7de43d2b756b17cb7f9201352e16145a1a1b5c6c7ea39c0d8fa253d1
+EBUILD kdm-9999.ebuild 3145 RMD160 5cd8ebd48792fd300204d62f19f0f4b26d4405ac SHA1 959d2b4d12979bc824cf2c08edf4aec7cdbed6df SHA256 e091478f7de43d2b756b17cb7f9201352e16145a1a1b5c6c7ea39c0d8fa253d1
 MISC metadata.xml 253 RMD160 dbe3d0cc1778dad287767c5dee6b46b42ccf20ec SHA1 b4723a2baaec997efee2cdc543248a2aad8f1a7e SHA256 955ffc185b3b01315be94542ea43ae1180d5f61ea539a317c45c5746b463eeba

kde-base/kdm/kdm-4.4.85.ebuild
@@ -100,15 +100,15 @@ pkg_postinst() {
 
 	# Set the default kdm face icon if it's not already set by the system admin
 	# because this is user-overrideable in that way, it's not in src_install
-	if [[ ! -e "${EROOT}${KDEDIR}/share/apps/kdm/faces/.default.face.icon" ]]; then
-		mkdir -p "${EROOT}${KDEDIR}/share/apps/kdm/faces"
+	if [[ ! -e "${EPREFIX}${KDM_HOME}/faces/.default.face.icon" ]]; then
+		mkdir -p "${EPREFIX}${KDM_HOME}/faces"
 		cp "${EROOT}${KDEDIR}/share/apps/kdm/pics/users/default1.png" \
-			"${EROOT}${KDEDIR}/share/apps/kdm/faces/.default.face.icon"
+			"${EPREFIX}${KDM_HOME}/faces/.default.face.icon"
 	fi
-	if [[ ! -e "${EROOT}${KDEDIR}/share/apps/kdm/faces/root.face.icon" ]]; then
-		mkdir -p "${EROOT}${KDEDIR}/share/apps/kdm/faces"
+	if [[ ! -e "${EPREFIX}${KDM_HOME}/faces/root.face.icon" ]]; then
+		mkdir -p "${EPREFIX}${KDM_HOME}/faces"
 		cp "${EROOT}${KDEDIR}/share/apps/kdm/pics/users/root1.png" \
-			"${EROOT}${KDEDIR}/share/apps/kdm/faces/root.face.icon"
+			"${EPREFIX}${KDM_HOME}/faces/root.face.icon"
 	fi
 
 	if use consolekit; then

kde-base/kdm/kdm-4.4.90.ebuild
@@ -103,7 +103,7 @@ pkg_postinst() {
 	if [[ ! -e "${EPREFIX}${KDM_HOME}/faces/.default.face.icon" ]]; then
 		mkdir -p "${EPREFIX}${KDM_HOME}/faces"
 		cp "${EROOT}${KDEDIR}/share/apps/kdm/pics/users/default1.png" \
-			"${PREFIX}${KDM_HOME}/faces/.default.face.icon"
+			"${EPREFIX}${KDM_HOME}/faces/.default.face.icon"
 	fi
 	if [[ ! -e "${EPREFIX}${KDM_HOME}/faces/root.face.icon" ]]; then
 		mkdir -p "${EPREFIX}${KDM_HOME}/faces"

kde-base/kdm/kdm-4.4.9999.ebuild
@@ -98,7 +98,7 @@ pkg_postinst() {
 	if [[ ! -e "${EPREFIX}${KDM_HOME}/faces/.default.face.icon" ]]; then
 		mkdir -p "${EPREFIX}${KDM_HOME}/faces"
 		cp "${EROOT}${KDEDIR}/share/apps/kdm/pics/users/default1.png" \
-			"${PREFIX}${KDM_HOME}/faces/.default.face.icon"
+			"${EPREFIX}${KDM_HOME}/faces/.default.face.icon"
 	fi
 	if [[ ! -e "${EPREFIX}${KDM_HOME}/faces/root.face.icon" ]]; then
 		mkdir -p "${EPREFIX}${KDM_HOME}/faces"

kde-base/kdm/kdm-4.5.9999.ebuild
@@ -102,7 +102,7 @@ pkg_postinst() {
 	if [[ ! -e "${EPREFIX}${KDM_HOME}/faces/.default.face.icon" ]]; then
 		mkdir -p "${EPREFIX}${KDM_HOME}/faces"
 		cp "${EROOT}${KDEDIR}/share/apps/kdm/pics/users/default1.png" \
-			"${PREFIX}${KDM_HOME}/faces/.default.face.icon"
+			"${EPREFIX}${KDM_HOME}/faces/.default.face.icon"
 	fi
 	if [[ ! -e "${EPREFIX}${KDM_HOME}/faces/root.face.icon" ]]; then
 		mkdir -p "${EPREFIX}${KDM_HOME}/faces"

kde-base/kdm/kdm-9999.ebuild
@@ -102,7 +102,7 @@ pkg_postinst() {
 	if [[ ! -e "${EPREFIX}${KDM_HOME}/faces/.default.face.icon" ]]; then
 		mkdir -p "${EPREFIX}${KDM_HOME}/faces"
 		cp "${EROOT}${KDEDIR}/share/apps/kdm/pics/users/default1.png" \
-			"${PREFIX}${KDM_HOME}/faces/.default.face.icon"
+			"${EPREFIX}${KDM_HOME}/faces/.default.face.icon"
 	fi
 	if [[ ! -e "${EPREFIX}${KDM_HOME}/faces/root.face.icon" ]]; then
 		mkdir -p "${EPREFIX}${KDM_HOME}/faces"