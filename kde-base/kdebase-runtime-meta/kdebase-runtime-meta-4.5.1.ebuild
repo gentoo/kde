gentoo/kde

commit 7a399515205ce5c9a26853ed9548220f3d97ac4f

[kde-base] Migrate >=4.5 from +handbook to KDE_HANDBOOK=1

kde-base/kdebase-runtime-meta/Manifest
@@ -1,5 +1,5 @@
 EBUILD kdebase-runtime-meta-4.4.9999.ebuild 1474 RMD160 78585c98fa1441d9414f502f5122a2babe4d6274 SHA1 8f1035f5742440da47613e277a36d359af2ba81d SHA256 d20cc7f1cce435be9da06d70359a2bb07cd3af3a6c927ab7e7c1c9d5a560e7c8
-EBUILD kdebase-runtime-meta-4.5.1.ebuild 1577 RMD160 c8e1b939370209bd60131089f2ef46bc7f548a97 SHA1 fcf6ca2c576be9ef7e713a39c8644fcbf3a82a9b SHA256 9c17fe3a19a0ae86105dde44361ff5d109ba77645690fe6ad778431b9d76dd69
-EBUILD kdebase-runtime-meta-4.5.9999.ebuild 1525 RMD160 4eb96c9fb99ea17c282306d0992b3313ea62882b SHA1 1362a39d0945002f7275cd9d3f49727a1ef491d5 SHA256 08d279222cdb21f8a8c5bd37034bbcf685c25e58e03ac50c175ce0b632535bc4
-EBUILD kdebase-runtime-meta-9999.ebuild 1526 RMD160 4549742222c3a12cd4139c39fc20032c7020a0d7 SHA1 b8ee7011b444ca6d08428342f49783737889fdb8 SHA256 2fd9b4e2ffee37061ebed845136f77a2c7b2fa5c537b931fa1bc703e4c26405b
+EBUILD kdebase-runtime-meta-4.5.1.ebuild 1567 RMD160 bfdb14597f5298baa6133c20a82d67f68aadc896 SHA1 91ab66cc8662dc4f7d6973cf7226ad0a89d79070 SHA256 2899dce69c8b6275a3a1f9c6a26a5226a8399d5ce0ff00ad8fc5735324291bc3
+EBUILD kdebase-runtime-meta-4.5.9999.ebuild 1515 RMD160 310287dbb929a63c289fe28c9fec4c9859b2ad40 SHA1 301f2a93d8a2f6fa9e547908dfa51ae628938448 SHA256 99c79a5c1c0425367edbc6881164dac69e929810374696ca11858449eaf17acf
+EBUILD kdebase-runtime-meta-9999.ebuild 1516 RMD160 90be4066dfd060bbc9693cbdf6d480e948fe3a39 SHA1 5b5fca5ecc6967df8be90ff32bd03d3ba07fbe83 SHA256 b0d8656c476873e670d1b99df389da5acb19fb6d0cf8b806b6c5a3909992355d
 MISC metadata.xml 156 RMD160 ecce3b981f150c45ae1e84e2d208e678d6124259 SHA1 b64f7c0b4e5db816d82ad19848f72118af129d35 SHA256 2f4da28506b9d4185f320f67a6191d30c7a921217ed4447ed46ea0bc4aefc79a

kde-base/kdebase-runtime-meta/kdebase-runtime-meta-4.5.1.ebuild
@@ -11,7 +11,7 @@ HOMEPAGE="http://www.kde.org/"
 LICENSE="GPL-2"
 SLOT="4.5"
 KEYWORDS="~amd64 ~arm ~ppc ~ppc64 ~x86 ~amd64-linux ~x86-linux"
-IUSE="aqua +handbook kdeprefix semantic-desktop"
+IUSE="aqua kdeprefix semantic-desktop"
 
 RDEPEND="
 	$(add_kdebase_dep activitymanager)

kde-base/kdebase-runtime-meta/kdebase-runtime-meta-4.5.9999.ebuild
@@ -11,7 +11,7 @@ HOMEPAGE="http://www.kde.org/"
 LICENSE="GPL-2"
 SLOT="4.5"
 KEYWORDS=""
-IUSE="aqua +handbook kdeprefix semantic-desktop"
+IUSE="aqua kdeprefix semantic-desktop"
 
 RDEPEND="
 	$(add_kdebase_dep activitymanager)

kde-base/kdebase-runtime-meta/kdebase-runtime-meta-9999.ebuild
@@ -11,7 +11,7 @@ HOMEPAGE="http://www.kde.org/"
 LICENSE="GPL-2"
 SLOT="live"
 KEYWORDS=""
-IUSE="aqua +handbook kdeprefix semantic-desktop"
+IUSE="aqua kdeprefix semantic-desktop"
 
 RDEPEND="
 	$(add_kdebase_dep activitymanager)

kde-base/kdelibs/Manifest
@@ -18,7 +18,7 @@ AUX kdelibs-4.4.90-xslt.patch 553 RMD160 b667c3330a2721ab0de237f50af81ec830954c0
 AUX kdelibs-4.5.0-kfilemetadatawidget.patch 1604 RMD160 a6ce10ad49190a779aabff86aef47579134ac279 SHA1 69ce54b4821859105717a6002444d2ba41e13632 SHA256 81da7bc2a364f67a22729b9ddd9fea30944ee5aaac768cd801adc0ce44fb21db
 DIST kdelibs-4.5.1.tar.bz2 13859768 RMD160 a1b7b68d484db7b5574188d5aacc6e7a43c6f8a9 SHA1 9d3eff7c35040bba6b6345a41e98a8e29ed2a1a3 SHA256 0dc482603bd894bbe194c2c84275af73602fe84932bc1ba9a92496cf57ce1bf7
 EBUILD kdelibs-4.4.9999.ebuild 9139 RMD160 795e65e22af5f09cfccbf6f3ed121d8bcdbb4f44 SHA1 31f3b50a494632318d0be78db3e2233c24433228 SHA256 fd4820a0ac773505665209c056ff0d2fc2dd49342871875fbb3dd42b802f2ed0
-EBUILD kdelibs-4.5.1.ebuild 9248 RMD160 37e15e6ceadc6585ec73351e596009b8dfaa4060 SHA1 22b3531fc4131c1d562a2c22b41848ef4bb88526 SHA256 55c917d9df31784d22b56a5816c1aa10599fa80f720e8728eb73b0c7bb36d4a9
-EBUILD kdelibs-4.5.9999.ebuild 9186 RMD160 5cde1fb4692c0e530f4daeea1b9bf1f0ce965676 SHA1 fecee0d40d4884563609349ea7077377e4022a47 SHA256 508d81b5f61729d4ec6dbbf7563de7726fbb870cc698bf59341d8cbd31ff8fc5
-EBUILD kdelibs-9999.ebuild 8969 RMD160 f45d81b474f8ec9b080275ca782e465cb8a2c289 SHA1 30eef976f2905043093058d92bb610665e7ca0a0 SHA256 c807654e8cbab64aa7f46be4b9cf9c3c2e8da1de7273584d62c0e1efebefe282
+EBUILD kdelibs-4.5.1.ebuild 9238 RMD160 1a4f5b09604382fc849d8611de049ecd063aa238 SHA1 336231a8774610ffce1d2e361910ab5b789b9ee2 SHA256 89c24fa480b13077f2c2cbf1121bcd891c4740f57068dc476e76d2e705936b55
+EBUILD kdelibs-4.5.9999.ebuild 9176 RMD160 1d2617c071c59f048019f890e5416ca9272e474f SHA1 feae89a608a3f487626f352a6fd2e3b6ca9852f8 SHA256 369f1384c1b5d87c76a5714bfaea46f4b9dde16cec30a9198071a68aaa61caf4
+EBUILD kdelibs-9999.ebuild 8959 RMD160 7c0e87a979da297675e85b96e12233fe8c26cd97 SHA1 183941193369fe71a83b70f1a14e7e596e9992f3 SHA256 f49776f28bd51fc8aa954eaac93a0a9547da22120dc0cffffcc58e65011e5782
 MISC metadata.xml 310 RMD160 29952f07b1829170319534936d11d39f1f7896bb SHA1 90ad5d77993322d4c25338f4005aaa4b642961c3 SHA256 1ce0cd7403862637e6e913bee33bf9f54a176bd1466d168be748e997a663095a

kde-base/kdelibs/kdelibs-4.5.1.ebuild
@@ -14,7 +14,7 @@ HOMEPAGE="http://www.kde.org/"
 
 KEYWORDS="~amd64 ~arm ~ppc ~ppc64 ~x86 ~x86-fbsd ~amd64-linux ~x86-linux"
 LICENSE="LGPL-2.1"
-IUSE="3dnow acl alsa altivec bindist +bzip2 debug doc fam +handbook jpeg2k kerberos
+IUSE="3dnow acl alsa altivec bindist +bzip2 debug doc fam jpeg2k kerberos
 lzma mmx nls openexr policykit semantic-desktop spell sse sse2 ssl zeroconf"
 
 # needs the kate regression testsuite from svn

kde-base/kdelibs/kdelibs-4.5.9999.ebuild
@@ -14,7 +14,7 @@ HOMEPAGE="http://www.kde.org/"
 
 KEYWORDS=""
 LICENSE="LGPL-2.1"
-IUSE="3dnow acl alsa altivec bindist +bzip2 debug doc fam +handbook jpeg2k kerberos
+IUSE="3dnow acl alsa altivec bindist +bzip2 debug doc fam jpeg2k kerberos
 lzma mmx nls openexr policykit semantic-desktop spell sse sse2 ssl zeroconf"
 
 # needs the kate regression testsuite from svn

kde-base/kdelibs/kdelibs-9999.ebuild
@@ -14,7 +14,7 @@ HOMEPAGE="http://www.kde.org/"
 
 KEYWORDS=""
 LICENSE="LGPL-2.1"
-IUSE="3dnow acl alsa altivec bindist +bzip2 debug doc fam +handbook jpeg2k kerberos
+IUSE="3dnow acl alsa altivec bindist +bzip2 debug doc fam jpeg2k kerberos
 lzma mmx nls openexr policykit semantic-desktop spell sse sse2 ssl zeroconf"
 
 # needs the kate regression testsuite from svn

commit 14be5c40cd68eae0fdb99014e227539e0377d20c

[kde-base/kdelibs,kdepimlibs,kdebase-runtime-meta] Fix ebuilds broken in 7a399515205ce5c9a26853ed9548220f3d97ac4f

kde-base/kdebase-runtime-meta/Manifest
@@ -1,5 +1,5 @@
 EBUILD kdebase-runtime-meta-4.4.9999.ebuild 1474 RMD160 78585c98fa1441d9414f502f5122a2babe4d6274 SHA1 8f1035f5742440da47613e277a36d359af2ba81d SHA256 d20cc7f1cce435be9da06d70359a2bb07cd3af3a6c927ab7e7c1c9d5a560e7c8
-EBUILD kdebase-runtime-meta-4.5.1.ebuild 1567 RMD160 bfdb14597f5298baa6133c20a82d67f68aadc896 SHA1 91ab66cc8662dc4f7d6973cf7226ad0a89d79070 SHA256 2899dce69c8b6275a3a1f9c6a26a5226a8399d5ce0ff00ad8fc5735324291bc3
-EBUILD kdebase-runtime-meta-4.5.9999.ebuild 1515 RMD160 310287dbb929a63c289fe28c9fec4c9859b2ad40 SHA1 301f2a93d8a2f6fa9e547908dfa51ae628938448 SHA256 99c79a5c1c0425367edbc6881164dac69e929810374696ca11858449eaf17acf
-EBUILD kdebase-runtime-meta-9999.ebuild 1516 RMD160 90be4066dfd060bbc9693cbdf6d480e948fe3a39 SHA1 5b5fca5ecc6967df8be90ff32bd03d3ba07fbe83 SHA256 b0d8656c476873e670d1b99df389da5acb19fb6d0cf8b806b6c5a3909992355d
+EBUILD kdebase-runtime-meta-4.5.1.ebuild 1577 RMD160 c8e1b939370209bd60131089f2ef46bc7f548a97 SHA1 fcf6ca2c576be9ef7e713a39c8644fcbf3a82a9b SHA256 9c17fe3a19a0ae86105dde44361ff5d109ba77645690fe6ad778431b9d76dd69
+EBUILD kdebase-runtime-meta-4.5.9999.ebuild 1525 RMD160 4eb96c9fb99ea17c282306d0992b3313ea62882b SHA1 1362a39d0945002f7275cd9d3f49727a1ef491d5 SHA256 08d279222cdb21f8a8c5bd37034bbcf685c25e58e03ac50c175ce0b632535bc4
+EBUILD kdebase-runtime-meta-9999.ebuild 1526 RMD160 4549742222c3a12cd4139c39fc20032c7020a0d7 SHA1 b8ee7011b444ca6d08428342f49783737889fdb8 SHA256 2fd9b4e2ffee37061ebed845136f77a2c7b2fa5c537b931fa1bc703e4c26405b
 MISC metadata.xml 156 RMD160 ecce3b981f150c45ae1e84e2d208e678d6124259 SHA1 b64f7c0b4e5db816d82ad19848f72118af129d35 SHA256 2f4da28506b9d4185f320f67a6191d30c7a921217ed4447ed46ea0bc4aefc79a

kde-base/kdebase-runtime-meta/kdebase-runtime-meta-4.5.1.ebuild
@@ -11,7 +11,7 @@ HOMEPAGE="http://www.kde.org/"
 LICENSE="GPL-2"
 SLOT="4.5"
 KEYWORDS="~amd64 ~arm ~ppc ~ppc64 ~x86 ~amd64-linux ~x86-linux"
-IUSE="aqua kdeprefix semantic-desktop"
+IUSE="aqua +handbook kdeprefix semantic-desktop"
 
 RDEPEND="
 	$(add_kdebase_dep activitymanager)

kde-base/kdebase-runtime-meta/kdebase-runtime-meta-4.5.9999.ebuild
@@ -11,7 +11,7 @@ HOMEPAGE="http://www.kde.org/"
 LICENSE="GPL-2"
 SLOT="4.5"
 KEYWORDS=""
-IUSE="aqua kdeprefix semantic-desktop"
+IUSE="aqua +handbook kdeprefix semantic-desktop"
 
 RDEPEND="
 	$(add_kdebase_dep activitymanager)

kde-base/kdebase-runtime-meta/kdebase-runtime-meta-9999.ebuild
@@ -11,7 +11,7 @@ HOMEPAGE="http://www.kde.org/"
 LICENSE="GPL-2"
 SLOT="live"
 KEYWORDS=""
-IUSE="aqua kdeprefix semantic-desktop"
+IUSE="aqua +handbook kdeprefix semantic-desktop"
 
 RDEPEND="
 	$(add_kdebase_dep activitymanager)

kde-base/kdelibs/Manifest
@@ -18,7 +18,7 @@ AUX kdelibs-4.4.90-xslt.patch 553 RMD160 b667c3330a2721ab0de237f50af81ec830954c0
 AUX kdelibs-4.5.0-kfilemetadatawidget.patch 1604 RMD160 a6ce10ad49190a779aabff86aef47579134ac279 SHA1 69ce54b4821859105717a6002444d2ba41e13632 SHA256 81da7bc2a364f67a22729b9ddd9fea30944ee5aaac768cd801adc0ce44fb21db
 DIST kdelibs-4.5.1.tar.bz2 13859768 RMD160 a1b7b68d484db7b5574188d5aacc6e7a43c6f8a9 SHA1 9d3eff7c35040bba6b6345a41e98a8e29ed2a1a3 SHA256 0dc482603bd894bbe194c2c84275af73602fe84932bc1ba9a92496cf57ce1bf7
 EBUILD kdelibs-4.4.9999.ebuild 9139 RMD160 795e65e22af5f09cfccbf6f3ed121d8bcdbb4f44 SHA1 31f3b50a494632318d0be78db3e2233c24433228 SHA256 fd4820a0ac773505665209c056ff0d2fc2dd49342871875fbb3dd42b802f2ed0
-EBUILD kdelibs-4.5.1.ebuild 9238 RMD160 1a4f5b09604382fc849d8611de049ecd063aa238 SHA1 336231a8774610ffce1d2e361910ab5b789b9ee2 SHA256 89c24fa480b13077f2c2cbf1121bcd891c4740f57068dc476e76d2e705936b55
-EBUILD kdelibs-4.5.9999.ebuild 9176 RMD160 1d2617c071c59f048019f890e5416ca9272e474f SHA1 feae89a608a3f487626f352a6fd2e3b6ca9852f8 SHA256 369f1384c1b5d87c76a5714bfaea46f4b9dde16cec30a9198071a68aaa61caf4
-EBUILD kdelibs-9999.ebuild 8959 RMD160 7c0e87a979da297675e85b96e12233fe8c26cd97 SHA1 183941193369fe71a83b70f1a14e7e596e9992f3 SHA256 f49776f28bd51fc8aa954eaac93a0a9547da22120dc0cffffcc58e65011e5782
+EBUILD kdelibs-4.5.1.ebuild 9253 RMD160 7c64209858f92021daaac28e7e9575618e33aec0 SHA1 09648f25aa55370c51343b7b8c2b8e267432b400 SHA256 9f5178905c5317ebff6bae80a22eb8f4824f822cc7e3a4d305bfbc785d204392
+EBUILD kdelibs-4.5.9999.ebuild 9191 RMD160 b131b9821e5861430db6eaab34845d8ef7eaa722 SHA1 38829f6a85b747a942a0838499ecf5b3f1d60600 SHA256 694a5bb7f63f9836f0feff85003f480769d586a7d360112396d7e925c36f91f3
+EBUILD kdelibs-9999.ebuild 8974 RMD160 0cac1c5aa94271cd5e28b80bd9a57b9ffe81733e SHA1 97aee82ba7f62c2817e7a0dd63fa39979e35d368 SHA256 f84a71445a6bfc3d3a2444554b49a39a6fa4625c3afbbb9049886eba5eab9e1f
 MISC metadata.xml 310 RMD160 29952f07b1829170319534936d11d39f1f7896bb SHA1 90ad5d77993322d4c25338f4005aaa4b642961c3 SHA256 1ce0cd7403862637e6e913bee33bf9f54a176bd1466d168be748e997a663095a

kde-base/kdelibs/kdelibs-4.5.1.ebuild
@@ -4,6 +4,7 @@
 
 EAPI="3"
 
+KDE_HANDBOOK=1
 CPPUNIT_REQUIRED="optional"
 OPENGL_REQUIRED="optional"
 WEBKIT_REQUIRED="always"

kde-base/kdelibs/kdelibs-4.5.9999.ebuild
@@ -4,6 +4,7 @@
 
 EAPI="3"
 
+KDE_HANDBOOK=1
 CPPUNIT_REQUIRED="optional"
 OPENGL_REQUIRED="optional"
 WEBKIT_REQUIRED="always"

kde-base/kdelibs/kdelibs-9999.ebuild
@@ -4,6 +4,7 @@
 
 EAPI="3"
 
+KDE_HANDBOOK=1
 CPPUNIT_REQUIRED="optional"
 OPENGL_REQUIRED="optional"
 WEBKIT_REQUIRED="always"

kde-base/kdepimlibs/Manifest
@@ -1,6 +1,6 @@
 DIST kdepimlibs-4.5.1.tar.bz2 2670072 RMD160 7c0ffb289be066469c052941543f84e214220f62 SHA1 a5e05e7a921e8fd7d719e9a3cd49b08385c6da7e SHA256 cb705522af5c55d94697daef6ce05c01829fa6ea00cfe5d8e38b1baf7ab79db6
 EBUILD kdepimlibs-4.4.9999.ebuild 1606 RMD160 6bb76ef94ebf7e7f5d0e2781a9988dd3018d5625 SHA1 4e35b9e1e12e3b28f1f119a6cdb6bae07f401c95 SHA256 157e5ef7185830957f2799beb20425e1efc1a724a7b2bd3ffbf4e0ebfcc70946
-EBUILD kdepimlibs-4.5.1.ebuild 2031 RMD160 9e9e4ccda8eb392f76525dc04d0abf9b46c59ec0 SHA1 5e5fa10add64395f00bb241ef57524b848b99852 SHA256 aba26192f583b71773858ca8bd4c25c88142e8f1111a1410616a9b73357fbcc4
-EBUILD kdepimlibs-4.5.9999.ebuild 1979 RMD160 14f105d4b9e9ea7f3ab386c9d0ca381bad1c36a7 SHA1 b46f2dd80382426a2e9eb8cc17a18db0906e380c SHA256 5147b4c6a39ab51caf850dbe1b2aa9c2cddbb9ab9c77673cc3a8bae0858312dc
-EBUILD kdepimlibs-9999.ebuild 1979 RMD160 14f105d4b9e9ea7f3ab386c9d0ca381bad1c36a7 SHA1 b46f2dd80382426a2e9eb8cc17a18db0906e380c SHA256 5147b4c6a39ab51caf850dbe1b2aa9c2cddbb9ab9c77673cc3a8bae0858312dc
+EBUILD kdepimlibs-4.5.1.ebuild 2046 RMD160 b6279800acfbea1ff483f5b94278de041ffa041e SHA1 3eb321bd7c4cc780ba3835ed7712444219dda820 SHA256 f5095b205611d523998838d9fad4205fb8980b8ef11428531bf88ff7e8fd4466
+EBUILD kdepimlibs-4.5.9999.ebuild 1994 RMD160 fdf6ecbb63499d5b90e140cb8206cffadefea01b SHA1 4d18b917a59f6b6b92d951260e326720af1f3fed SHA256 bd71c259b8786ba736f2ce91becc23e54f55a154be912ec71a9e65ef12408ec1
+EBUILD kdepimlibs-9999.ebuild 1994 RMD160 fdf6ecbb63499d5b90e140cb8206cffadefea01b SHA1 4d18b917a59f6b6b92d951260e326720af1f3fed SHA256 bd71c259b8786ba736f2ce91becc23e54f55a154be912ec71a9e65ef12408ec1
 MISC metadata.xml 156 RMD160 ecce3b981f150c45ae1e84e2d208e678d6124259 SHA1 b64f7c0b4e5db816d82ad19848f72118af129d35 SHA256 2f4da28506b9d4185f320f67a6191d30c7a921217ed4447ed46ea0bc4aefc79a

kde-base/kdepimlibs/kdepimlibs-4.5.1.ebuild
@@ -4,6 +4,7 @@
 
 EAPI="3"
 
+KDE_HANDBOOK=1
 CPPUNIT_REQUIRED="optional"
 inherit kde4-base
 

kde-base/kdepimlibs/kdepimlibs-4.5.9999.ebuild
@@ -4,6 +4,7 @@
 
 EAPI="3"
 
+KDE_HANDBOOK=1
 CPPUNIT_REQUIRED="optional"
 inherit kde4-base
 

kde-base/kdepimlibs/kdepimlibs-9999.ebuild
@@ -4,6 +4,7 @@
 
 EAPI="3"
 
+KDE_HANDBOOK=1
 CPPUNIT_REQUIRED="optional"
 inherit kde4-base
 