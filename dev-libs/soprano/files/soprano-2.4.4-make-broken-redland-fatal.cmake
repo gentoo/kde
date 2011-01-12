145664180dd79582fd34b781783525d16027329f
Theo Chatzimichos <tampakrap@gentoo.org>
2011-01-12 22:14:10 +0200

    [dev-libs/soprano] Remove clucene2(java) support

--- dev-libs/soprano/files/soprano-2.4.4-make-broken-redland-fatal.cmake
+++ dev-libs/soprano/files/soprano-2.4.4-make-broken-redland-fatal.cmake
@@ -1,6 +1,6 @@
-diff -ru ../soprano-2.4.4/cmake/modules/FindRedland.cmake ./cmake/modules/FindRedland.cmake
---- ../soprano-2.4.4/cmake/modules/FindRedland.cmake	2009-12-18 15:33:44.000000000 +0100
-+++ ./cmake/modules/FindRedland.cmake	2010-07-06 03:25:55.421484970 +0200
+diff -ru cmake/modules/FindRedland.cmake cmake/modules/FindRedland.cmake
+--- cmake/modules/FindRedland.cmake	2009-12-18 15:33:44.000000000 +0100
++++ cmake/modules/FindRedland.cmake	2010-07-06 03:25:55.421484970 +0200
 @@ -108,7 +108,7 @@
      if(NOT "${_TEST_EXITCODE}" EQUAL 0)
        set(_REDLAND_VERSION_OK)
