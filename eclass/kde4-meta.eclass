gentoo/kde

commit 034e30c17e5c9171ccd1022818e101c5e7b6c934

[eclass] Remove dead code.

eclass/kde4-meta.eclass
@@ -308,21 +308,6 @@ kde4-meta_create_extractlists() {
 			fi
 			;;
 	esac
-	# Don't install cmake modules for split ebuilds, to avoid collisions.
-	# note: kdegraphics >= 4.6.2 does not even have code to do that, so we
-	#   should not try in that case
-	# note2: kdeedu 4.6.4 does not have a cmake/modules/ subdir anymore :(
-	#   it may be possible to formulate this shorter, but it should also
-	#   still be understandable...
-	if [[ ${KMNAME} != kdegraphics || ( ( $(get_kde_version) != 4.6 || ${PV} < 4.6.2 ) && $(get_kde_version) < 4.7 ) ]] \
-		&& ! [[ ${KMNAME} == kdeedu && ( ${PV} == 4.6.4 || ${PV} == 4.6.5 ) ]]; then
-		case ${KMNAME} in
-			kdebase-runtime|kde-runtime|kde-workspace|kdeedu|kdegraphics)
-				KMEXTRACTONLY+="
-					cmake/modules/"
-			;;
-		esac
-	fi
 
 	debug-print "line ${LINENO} ${ECLASS} ${FUNCNAME}: KMEXTRACTONLY ${KMEXTRACTONLY}"
 }

commit aed201a7d615ef4c23273554cd44188c2d5ced00

[eclass] Don't install cmake modules for kde-runtime and kde-workspace.
This fixes a regression caused by 034e30c17e5c9171ccd1022818e101c5e7b6c934.

eclass/kde4-meta.eclass
@@ -279,11 +279,13 @@ kde4-meta_create_extractlists() {
 			;;
 		kde-runtime)
 			KMEXTRACTONLY+="
+				cmake/modules/
 				CTestConfig.cmake
 				config-runtime.h.cmake"
 			;;
 		kde-workspace)
 			KMEXTRACTONLY+="
+				cmake/modules/
 				config-unix.h.cmake
 				ConfigureChecks.cmake
 				config-workspace.h.cmake