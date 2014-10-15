gentoo/kde

commit 500fc8f89fc3951ca0581c347ef5130a3a171ced

Move KF5 libkscreen from x11-libs to kde-base as it is part of Plasma 5 proper.

kde-base/libkscreen/libkscreen-9999.ebuild
@@ -5,20 +5,11 @@
 EAPI=5
 
 VIRTUALX_REQUIRED="test"
-if [[ $PV = *9999* ]]; then
-	EGIT_BRANCH="frameworks"
-	KEYWORDS=""
-else
-	SRC_URI="mirror://kde/stable/plasma/${PV}/${P}.tar.xz"
-	KEYWORDS="~amd64 ~x86"
-fi
-
 inherit kde5
 
 DESCRIPTION="KDE screen management library"
-HOMEPAGE="https://projects.kde.org/projects/extragear/libs/libkscreen"
-
 LICENSE="GPL-2"
+KEYWORDS=""
 IUSE=""
 
 # TODO: add X use flag, does not build at the moment
@@ -30,4 +21,6 @@ DEPEND="
 	x11-libs/libxcb
 	x11-libs/libXrandr
 "
-RDEPEND="${DEPEND}"
+RDEPEND="${DEPEND}
+	!x11-libs/libkscreen:5
+"

Documentation/package.accept_keywords/kde-plasma-5.0.keywords
@@ -13,6 +13,7 @@
 <kde-base/ksysguard-5.0.50:5
 <kde-base/kwin-5.0.50:5
 <kde-base/kwrited-5.0.50:5
+<kde-base/libkscreen-5.0.50:5
 <kde-base/libksysguard-5.0.50:5
 <kde-base/milou-5.0.50:5
 <kde-base/oxygen-5.0.50:5

Documentation/package.mask/kde-plasma-5.0
@@ -13,6 +13,7 @@
 >=kde-base/ksysguard-5.0:5
 >=kde-base/kwin-5.0:5
 >=kde-base/kwrited-5.0:5
+>=kde-base/libkscreen-5.0:5
 >=kde-base/libksysguard-5.0:5
 >=kde-base/milou-5.0:5
 >=kde-base/oxygen-5.0:5

Documentation/package.unmask/kde-plasma-5.0
@@ -13,6 +13,7 @@
 <kde-base/ksysguard-5.0.50:5
 <kde-base/kwin-5.0.50:5
 <kde-base/kwrited-5.0.50:5
+<kde-base/libkscreen-5.0.50:5
 <kde-base/libksysguard-5.0.50:5
 <kde-base/milou-5.0.50:5
 <kde-base/oxygen-5.0.50:5

kde-base/libkscreen/libkscreen-5.0.93.ebuild
@@ -9,7 +9,7 @@ if [[ $PV = *9999* ]]; then
 	EGIT_BRANCH="frameworks"
 	KEYWORDS=""
 else
-	SRC_URI="mirror://kde/unstable/plasma/${PV}/${P}.tar.xz"
+	SRC_URI="mirror://kde/stable/plasma/5.0.2/${P}.tar.xz"
 	KEYWORDS="~amd64 ~x86"
 fi
 
@@ -30,4 +30,6 @@ DEPEND="
 	x11-libs/libxcb
 	x11-libs/libXrandr
 "
-RDEPEND="${DEPEND}"
+RDEPEND="${DEPEND}
+	x11-libs/libkscreen:5
+"

kde-base/libkscreen/libkscreen-5.0.95.ebuild
@@ -1,0 +1,35 @@
+# Copyright 1999-2014 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+# $Header: $
+
+EAPI=5
+
+VIRTUALX_REQUIRED="test"
+if [[ $PV = *9999* ]]; then
+	EGIT_BRANCH="frameworks"
+	KEYWORDS=""
+else
+	SRC_URI="mirror://kde/unstable/plasma/${PV}/${P}.tar.xz"
+	KEYWORDS="~amd64 ~x86"
+fi
+
+inherit kde5
+
+DESCRIPTION="KDE screen management library"
+HOMEPAGE="https://projects.kde.org/projects/extragear/libs/libkscreen"
+
+LICENSE="GPL-2"
+IUSE=""
+
+# TODO: add X use flag, does not build at the moment
+
+DEPEND="
+	dev-qt/qtgui:5
+	dev-qt/qtx11extras:5
+	x11-libs/libX11
+	x11-libs/libxcb
+	x11-libs/libXrandr
+"
+RDEPEND="${DEPEND}
+	!x11-libs/libkscreen:5
+"

kde-base/libkscreen/libkscreen-5.1.0.ebuild
@@ -5,20 +5,11 @@
 EAPI=5
 
 VIRTUALX_REQUIRED="test"
-if [[ $PV = *9999* ]]; then
-	EGIT_BRANCH="frameworks"
-	KEYWORDS=""
-else
-	SRC_URI="mirror://kde/stable/plasma/5.0.2/${P}.tar.xz"
-	KEYWORDS="~amd64 ~x86"
-fi
-
 inherit kde5
 
 DESCRIPTION="KDE screen management library"
-HOMEPAGE="https://projects.kde.org/projects/extragear/libs/libkscreen"
-
 LICENSE="GPL-2"
+KEYWORDS=" ~amd64"
 IUSE=""
 
 # TODO: add X use flag, does not build at the moment
@@ -30,4 +21,6 @@ DEPEND="
 	x11-libs/libxcb
 	x11-libs/libXrandr
 "
-RDEPEND="${DEPEND}"
+RDEPEND="${DEPEND}
+	!x11-libs/libkscreen:5
+"

commit eacc9917f63a967ba9742ac0a93fa2adf8b1054d

[kde-base/libkscreen] add EGIT_BRANCH
This fixes a regression introduced by 500fc8f89fc3951ca0581c347ef5130a3a171ced.

kde-base/libkscreen/libkscreen-9999.ebuild
@@ -5,6 +5,7 @@
 EAPI=5
 
 VIRTUALX_REQUIRED="test"
+EGIT_BRANCH="frameworks"
 inherit kde5
 
 DESCRIPTION="KDE screen management library"