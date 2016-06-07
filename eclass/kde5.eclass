gentoo/kde

commit 40df59420cf5e14a4fdb7e0b282c79322fa88f62

kde5.eclass: Add handling of building plugins for Qt5Designer
KDE_DESIGNERPLUGIN toggles dependencies and configure with USE=designer

eclass/kde5.eclass
@@ -56,6 +56,13 @@ EXPORT_FUNCTIONS pkg_setup pkg_nofetch src_unpack src_prepare src_configure src_
 # Otherwise, add debug to IUSE to control building with that flag.
 : ${KDE_DEBUG:=true}
 
+# @ECLASS-VARIABLE: KDE_DESIGNERPLUGIN
+# @DESCRIPTION:
+# If set to "false", do nothing.
+# Otherwise, add "designer" to IUSE to toggle build of designer plugins
+# and add the necessary DEPENDs.
+: ${KDE_DESIGNERPLUGIN:=false}
+
 # @ECLASS-VARIABLE: KDE_DOXYGEN
 # @DESCRIPTION:
 # If set to "false", do nothing.
@@ -205,6 +212,17 @@ case ${KDE_DEBUG} in
 	false)	;;
 	*)
 		IUSE+=" debug"
+		;;
+esac
+
+case ${KDE_DESIGNERPLUGIN} in
+	false)  ;;
+	*)
+		IUSE+=" designer"
+		DEPEND+=" designer? (
+			$(add_frameworks_dep kdesignerplugin)
+			$(add_qt_dep designer)
+		)"
 		;;
 esac
 
@@ -617,6 +635,10 @@ kde5_src_configure() {
 
 	if ! use_if_iuse handbook && [[ ${KDE_HANDBOOK} = optional ]] ; then
 		cmakeargs+=( -DCMAKE_DISABLE_FIND_PACKAGE_KF5DocTools=ON )
+	fi
+
+	if ! use_if_iuse designer ; then
+		cmakeargs+=( -DCMAKE_DISABLE_FIND_PACKAGE_Qt5Designer=ON )
 	fi
 
 	# install mkspecs in the same directory as qt stuff

commit b5a59f28acd9cc9212a0ef39103251606777fa63

kde5.eclass: Fix find designer plugin condition
Regression introduced in 40df59420cf5e14a4fdb7e0b282c79322fa88f62. New
global var for creating a designer use flag is per default false. If
designer use flag is not set, finding designer package is hard disabled
unconditionally.

eclass/kde5.eclass
@@ -637,7 +637,7 @@ kde5_src_configure() {
 		cmakeargs+=( -DCMAKE_DISABLE_FIND_PACKAGE_KF5DocTools=ON )
 	fi
 
-	if ! use_if_iuse designer ; then
+	if ! use_if_iuse designer && [[ ${KDE_DESIGNERPLUGIN} != false ]] ; then
 		cmakeargs+=( -DCMAKE_DISABLE_FIND_PACKAGE_Qt5Designer=ON )
 	fi
 