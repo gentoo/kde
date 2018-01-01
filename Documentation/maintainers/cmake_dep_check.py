d057ce3c3a19c2416761f395eb726e76f2f7f7fb
Andreas Sturmlechner <asturm@gentoo.org>
2018-01-01 01:26:00 +0100

    */*: Welcome to 2018!
    Package-Manager: Portage-2.3.19, Repoman-2.3.6

--- Documentation/maintainers/cmake_dep_check.py
+++ Documentation/maintainers/cmake_dep_check.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright (c) 2017 Michael Palimaka <kensington@gentoo.org>
+# Copyright (c) 2018 Michael Palimaka <kensington@gentoo.org>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
