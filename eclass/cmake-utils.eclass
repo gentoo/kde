gentoo/kde

commit a8d5b1da360ab6e7a09bceda8a34d37e2182b963

cmake-utils.eclass: port has_version usage handle EAPI=7

eclass/cmake-utils.eclass
@@ -226,9 +226,18 @@ _cmake_generator_to_use() {
 		ninja)
 			# if ninja is enabled but not installed, the build could fail
 			# this could happen if ninja is manually enabled (eg. make.conf) but not installed
-			if ! ROOT=/ has_version dev-util/ninja; then
-				die "CMAKE_MAKEFILE_GENERATOR is set to ninja, but ninja is not installed. Please install dev-util/ninja or unset CMAKE_MAKEFILE_GENERATOR."
-			fi
+			case ${EAPI} in
+				5|6)
+					if ! ROOT=/ has_version dev-util/ninja; then
+						die "CMAKE_MAKEFILE_GENERATOR is set to ninja, but ninja is not installed. Please install dev-util/ninja or unset CMAKE_MAKEFILE_GENERATOR."
+					fi
+				;;
+				*)
+					if ! has_version -b dev-util/ninja; then
+						die "CMAKE_MAKEFILE_GENERATOR is set to ninja, but ninja is not installed. Please install dev-util/ninja or unset CMAKE_MAKEFILE_GENERATOR."
+					fi
+				;;
+			esac
 			generator_name="Ninja"
 			;;
 		emake)
@@ -511,11 +520,23 @@ cmake-utils_src_configure() {
 	# Since cmake-3.4.0_rc1 "<FLAGS>" no longer contains includes and thus
 	# we need to add "<INCLUDES>"
 	local includes=
+
+	local has_cmake_340=
+	case ${EAPI} in
+		5|6)
+			has_cmake_34=$(ROOT=/ has_version \>=dev-util/cmake-3.4.0_rc1)
+		;;
+		*)
+			has_cmake_34=$(has_version -b \>=dev-util/cmake-3.4.0_rc1)
+		;;
+	esac
+
+
 	if [[ ${PN} == cmake ]] ; then
 		if $(ver_test $(ver_cut 1-3 ${PV}) -ge 3.4.0) ; then
 			includes="<INCLUDES>"
 		fi
-	elif ROOT=/ has_version \>=dev-util/cmake-3.4.0_rc1 ; then
+	elif [[ ${has_cmake_340} ]]; then
 		includes="<INCLUDES>"
 	fi
 	cat > "${build_rules}" <<- _EOF_ || die

commit b6c48cc0aae681ff9b5e9e67398002408dbc3fe5

cmake-utils.eclass: Fix usage of has_version.
has_version has no output, and its result is provided by exit status.

Fixes: a8d5b1da360ab6e7a09bceda8a34d37e2182b963

eclass/cmake-utils.eclass
@@ -521,14 +521,14 @@ cmake-utils_src_configure() {
 	# we need to add "<INCLUDES>"
 	local includes=
 
-	local has_cmake_340=
+	local has_cmake_3_4_0=false
 	case ${EAPI} in
 		5|6)
-			has_cmake_34=$(ROOT=/ has_version \>=dev-util/cmake-3.4.0_rc1)
-		;;
+			ROOT=/ has_version ">=dev-util/cmake-3.4.0_rc1" && has_cmake_3_4_0=true
+			;;
 		*)
-			has_cmake_34=$(has_version -b \>=dev-util/cmake-3.4.0_rc1)
-		;;
+			has_version -b ">=dev-util/cmake-3.4.0_rc1" && has_cmake_3_4_0=true
+			;;
 	esac
 
 
@@ -536,7 +536,7 @@ cmake-utils_src_configure() {
 		if $(ver_test $(ver_cut 1-3 ${PV}) -ge 3.4.0) ; then
 			includes="<INCLUDES>"
 		fi
-	elif [[ ${has_cmake_340} ]]; then
+	elif ${has_cmake_3_4_0}; then
 		includes="<INCLUDES>"
 	fi
 	cat > "${build_rules}" <<- _EOF_ || die