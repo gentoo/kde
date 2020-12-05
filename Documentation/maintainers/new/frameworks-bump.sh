d291366f7e88c3ec5865249d44f7841b8a696e3a
Andreas Sturmlechner <asturm@gentoo.org>
2020-12-05 16:53:12 +0100

    Documentation: frameworks-bump.sh: Follow-up create_keywords_files() change
    Signed-off-by: Andreas Sturmlechner <asturm@gentoo.org>

--- Documentation/maintainers/new/frameworks-bump.sh
+++ Documentation/maintainers/new/frameworks-bump.sh
@@ -42,7 +42,7 @@ pushd "${TARGET_REPO}" > /dev/null
 bump_set_from_live kde-frameworks ${major_version}
 mask_from_live_set kde-frameworks ${VERSION} ${kfv}
 mark_unreleased ${kfv}
-create_keywords_files kde-frameworks-live ${kfmv}
+create_keywords_files ${kfmv}
 
 sed -i -e "/KF_RELEASES/s/\"$/ ${major_version}\"/" Documentation/maintainers/regenerate-files
 Documentation/maintainers/regenerate-files
