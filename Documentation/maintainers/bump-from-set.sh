18888aa174bf206487c4079e57f488d871de11e6
Andreas Sturmlechner <asturm@gentoo.org>
2020-12-05 21:43:03 +0100

    Documentation: bump-from-set.sh: Fix get_main_tree_keyword()
    Sometimes ^KEYWORDS does not match.
    
    Signed-off-by: Andreas Sturmlechner <asturm@gentoo.org>

--- Documentation/maintainers/bump-from-set.sh
+++ Documentation/maintainers/bump-from-set.sh
@@ -27,7 +27,7 @@ get_main_tree_keyword() {
 	local portdir="$(portageq get_repo_path / gentoo)"
 	local cp="${1}"
 	if [[ -e ${portdir}/${cp} ]] ; then
-		echo $(sed -ne 's/^KEYWORDS="\(.*\)"/\1/p' "$(ls ${portdir}/${cp}/*.ebuild | sort | tail -n 1)")
+		echo $(sed -ne 's/^\s*KEYWORDS="\(.*\)"/\1/p' "$(ls ${portdir}/${cp}/*.ebuild | sort | tail -n 1)")
 	else
 		echo "~amd64 ~x86"
 	fi
