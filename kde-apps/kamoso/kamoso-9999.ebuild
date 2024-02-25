gentoo/kde

commit 40468034431f090d7806623f2b0fc464bb67a107

kde-apps/kamoso: attempt Qt6 (but not KF6) port
Upstream added Qt6 support in c9e203a3d72c4671f6d2673818654981738def2c but
no KF6 support. I guess let's try it?

Signed-off-by: Sam James <sam@gentoo.org>

kde-apps/kamoso/kamoso-9999.ebuild
@@ -6,14 +6,14 @@ EAPI=8
 ECM_TEST="true"
 ECM_HANDBOOK="forceoptional"
 KFMIN=5.113.0
-QTMIN=5.15.9
+QTMIN=6.6.2
 inherit ecm gear.kde.org
 
 DESCRIPTION="Application to take pictures and videos from your webcam by KDE"
 HOMEPAGE="https://apps.kde.org/kamoso/ https://userbase.kde.org/Kamoso"
 
 LICENSE="GPL-2+"
-SLOT="5"
+SLOT="6"
 KEYWORDS=""
 IUSE=""
 
@@ -21,9 +21,8 @@ RESTRICT="test" # bug 653674
 
 COMMON_DEPEND="
 	dev-libs/glib:2
-	>=dev-qt/qtdeclarative-${QTMIN}:5
-	>=dev-qt/qtgui-${QTMIN}:5
-	>=dev-qt/qtwidgets-${QTMIN}:5
+	>=dev-qt/qtbase-${QTMIN}:6[gui,widgets]
+	>=dev-qt/qtdeclarative-${QTMIN}:6
 	>=kde-frameworks/kconfig-${KFMIN}:5
 	>=kde-frameworks/kcoreaddons-${KFMIN}:5
 	>=kde-frameworks/ki18n-${KFMIN}:5
@@ -34,10 +33,10 @@ COMMON_DEPEND="
 	virtual/opengl
 "
 DEPEND="${COMMON_DEPEND}
-	test? ( >=dev-qt/qtopengl-${QTMIN}:5 )
+	test? ( >=dev-qt/qtbase-${QTMIN}:6[opengl] )
 "
 RDEPEND="${COMMON_DEPEND}
-	>=dev-qt/qtquickcontrols2-${QTMIN}:5
+	>=dev-qt/qtdeclarative-${QTMIN}:6
 	>=kde-frameworks/kirigami-${KFMIN}:5
 	media-plugins/gst-plugins-jpeg:1.0
 	media-plugins/gst-plugins-libpng:1.0

commit 555f35d2ab3ced59790a75e595f4acfc1c96efa8

kde-apps/kamoso: fix SLOT for 9999
Fixes: 40468034431f090d7806623f2b0fc464bb67a107
Signed-off-by: Sam James <sam@gentoo.org>

kde-apps/kamoso/kamoso-9999.ebuild
@@ -13,7 +13,7 @@ DESCRIPTION="Application to take pictures and videos from your webcam by KDE"
 HOMEPAGE="https://apps.kde.org/kamoso/ https://userbase.kde.org/Kamoso"
 
 LICENSE="GPL-2+"
-SLOT="6"
+SLOT="5"
 KEYWORDS=""
 IUSE=""
 