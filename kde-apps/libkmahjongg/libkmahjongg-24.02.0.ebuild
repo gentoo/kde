gentoo/kde

commit 04beb2da9b649dba3a6ccc5bf4349a52947c20fd

kde-apps/libkmahjongg: refresh deps
* Drop kcoreaddons for upstream commit 201e1d643f88fa3674020ac321ca9df0cc66220c
* Drop kcompletion for upstream commit b95dc326013e5324fbd52037e7233be521308654

Signed-off-by: Sam James <sam@gentoo.org>

kde-apps/libkmahjongg/libkmahjongg-24.02.0.ebuild
@@ -11,16 +11,14 @@ DESCRIPTION="Mahjongg library based on Qt/KDE Frameworks"
 
 LICENSE="GPL-2" # TODO: CHECK
 SLOT="6"
-KEYWORDS="~amd64"
+KEYWORDS=""
 IUSE=""
 
 DEPEND="
 	>=dev-qt/qtbase-${QTMIN}:6[gui,widgets]
 	>=dev-qt/qtsvg-${QTMIN}:6
-	>=kde-frameworks/kcompletion-${KFMIN}:6
 	>=kde-frameworks/kconfig-${KFMIN}:6
 	>=kde-frameworks/kconfigwidgets-${KFMIN}:6
-	>=kde-frameworks/kcoreaddons-${KFMIN}:6
 	>=kde-frameworks/ki18n-${KFMIN}:6
 	>=kde-frameworks/kwidgetsaddons-${KFMIN}:6
 "

kde-apps/libkmahjongg/libkmahjongg-24.02.49.9999.ebuild
@@ -17,10 +17,8 @@ IUSE=""
 DEPEND="
 	>=dev-qt/qtbase-${QTMIN}:6[gui,widgets]
 	>=dev-qt/qtsvg-${QTMIN}:6
-	>=kde-frameworks/kcompletion-${KFMIN}:6
 	>=kde-frameworks/kconfig-${KFMIN}:6
 	>=kde-frameworks/kconfigwidgets-${KFMIN}:6
-	>=kde-frameworks/kcoreaddons-${KFMIN}:6
 	>=kde-frameworks/ki18n-${KFMIN}:6
 	>=kde-frameworks/kwidgetsaddons-${KFMIN}:6
 "

kde-apps/libkmahjongg/libkmahjongg-9999.ebuild
@@ -17,10 +17,8 @@ IUSE=""
 DEPEND="
 	>=dev-qt/qtbase-${QTMIN}:6[gui,widgets]
 	>=dev-qt/qtsvg-${QTMIN}:6
-	>=kde-frameworks/kcompletion-${KFMIN}:6
 	>=kde-frameworks/kconfig-${KFMIN}:6
 	>=kde-frameworks/kconfigwidgets-${KFMIN}:6
-	>=kde-frameworks/kcoreaddons-${KFMIN}:6
 	>=kde-frameworks/ki18n-${KFMIN}:6
 	>=kde-frameworks/kwidgetsaddons-${KFMIN}:6
 "

commit 32819420c0510f0825bcb0d085e2ab87a334f393

kde-apps/libkmahjongg: keyword 24.02.0 for ~amd64
Fixes: 04beb2da9b649dba3a6ccc5bf4349a52947c20fd
Signed-off-by: Sam James <sam@gentoo.org>

kde-apps/libkmahjongg/libkmahjongg-24.02.0.ebuild
@@ -11,7 +11,7 @@ DESCRIPTION="Mahjongg library based on Qt/KDE Frameworks"
 
 LICENSE="GPL-2" # TODO: CHECK
 SLOT="6"
-KEYWORDS=""
+KEYWORDS="~amd64"
 IUSE=""
 
 DEPEND="