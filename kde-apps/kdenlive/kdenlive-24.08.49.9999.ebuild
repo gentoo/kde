gentoo/kde

commit 69e7cbec2adbd87e6795939880c76ec4fa033254

kde-apps/kdenlive: add linux-headers dep
Uses linux/input.h.

Signed-off-by: Sam James <sam@gentoo.org>

kde-apps/kdenlive/kdenlive-24.08.49.9999.ebuild
@@ -61,6 +61,7 @@ RDEPEND="${DEPEND}
 	media-video/mediainfo
 "
 BDEPEND="sys-devel/gettext"
+DEPEND+=" sys-kernel/linux-headers"
 
 src_configure() {
 	local mycmakeargs=(

kde-apps/kdenlive/kdenlive-9999.ebuild
@@ -61,6 +61,7 @@ RDEPEND="${DEPEND}
 	media-video/mediainfo
 "
 BDEPEND="sys-devel/gettext"
+DEPEND+=" sys-kernel/linux-headers"
 
 src_configure() {
 	local mycmakeargs=(

commit 0c2266d470a965579402cbac84375d6f70ccb3c1

kde-apps/kdenlive: use virtual/os-headers instead
The use of linux-headers in kdenlive isn't CMake REQUIRED so use
virtual/os-headers to avoid needing to write 'kernel_linux? ( ... )'.

AFAIK, many (if not all) uses of os-headers are actually for that,
rather than saying "I use headers from the OS, really, not just Linux".

Fixes: 69e7cbec2adbd87e6795939880c76ec4fa033254
Signed-off-by: Sam James <sam@gentoo.org>

kde-apps/kdenlive/kdenlive-24.08.49.9999.ebuild
@@ -61,7 +61,7 @@ RDEPEND="${DEPEND}
 	media-video/mediainfo
 "
 BDEPEND="sys-devel/gettext"
-DEPEND+=" sys-kernel/linux-headers"
+DEPEND+=" virtual/os-headers"
 
 src_configure() {
 	local mycmakeargs=(

kde-apps/kdenlive/kdenlive-9999.ebuild
@@ -61,7 +61,7 @@ RDEPEND="${DEPEND}
 	media-video/mediainfo
 "
 BDEPEND="sys-devel/gettext"
-DEPEND+=" sys-kernel/linux-headers"
+DEPEND+=" virtual/os-headers"
 
 src_configure() {
 	local mycmakeargs=(