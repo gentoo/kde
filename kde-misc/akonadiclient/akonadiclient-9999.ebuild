gentoo/kde

commit d3bf79f39b6bce621e7e7260173cb92d374b5947

kde-misc/akonadiclient: Port to KF6
Upstream commit 81d8a18fb677391c3c30df1e4e9a57f84cd5a6d1

Signed-off-by: Andreas Sturmlechner <asturm@gentoo.org>

kde-misc/akonadiclient/akonadiclient-9999.ebuild
@@ -4,26 +4,28 @@
 EAPI=8
 
 ECM_HANDBOOK="forceoptional"
-ECM_TEST="forceoptional"
-KFMIN=5.115.0
-QTMIN=5.15.12
+ECM_TEST="true"
+KFMIN=6.5.0
+QTMIN=6.7.2
 inherit ecm kde.org
 
 DESCRIPTION="Commandline interface for accessing Akonadi"
 HOMEPAGE="https://invent.kde.org/pim/akonadiclient/"
 
 LICENSE="GPL-2"
-SLOT="5"
+SLOT="6"
 KEYWORDS=""
 IUSE=""
 
 DEPEND="
-	>=dev-qt/qtwidgets-${QTMIN}:5
-	kde-apps/akonadi:5[xml]
-	kde-apps/kmime:5
-	>=kde-frameworks/kcodecs-${KFMIN}:5
-	>=kde-frameworks/kcontacts-${KFMIN}:5
-	>=kde-frameworks/kcoreaddons-${KFMIN}:5
-	>=kde-frameworks/ki18n-${KFMIN}:5
+	>=dev-qt/qtbase-${QTMIN}:6[widgets]
+	kde-apps/akonadi:6[xml]
+	kde-apps/kmime:6
+	>=kde-frameworks/kcodecs-${KFMIN}:6
+	>=kde-frameworks/kcontacts-${KFMIN}:6
+	>=kde-frameworks/kcoreaddons-${KFMIN}:6
+	>=kde-frameworks/ki18n-${KFMIN}:6
 "
-RDEPEND="${DEPEND}"
+RDEPEND="${DEPEND}
+	!${CATEGORY}/${PN}:5
+"

commit 7e4e5bfaaefe5462334128fe092c5f8400c7df60

kde-misc/akonadiclient: missing dependency akonadi-contacts
Fixes: d3bf79f39b6bce621e7e7260173cb92d374b5947
Signed-off-by: Alfred Wingate <parona@protonmail.com>

kde-misc/akonadiclient/akonadiclient-9999.ebuild
@@ -20,6 +20,7 @@ IUSE=""
 DEPEND="
 	>=dev-qt/qtbase-${QTMIN}:6[widgets]
 	kde-apps/akonadi:6[xml]
+	kde-apps/akonadi-contacts:6
 	kde-apps/kmime:6
 	>=kde-frameworks/kconfig-${KFMIN}:6
 	>=kde-frameworks/kcodecs-${KFMIN}:6