gentoo/kde

commit 2b20c2d1d871eabb72bdace5c21b5963fab7a85e

kde-apps/dragon: refresh dependencies
Upstream commit c951fff6635d0ac24a71469171a505a1d7878426. Note that
kirigami is REQUIRED (as is qtdeclarative) so not just RDEPEND.

Signed-off-by: Sam James <sam@gentoo.org>

kde-apps/dragon/dragon-9999.ebuild
@@ -17,22 +17,16 @@ KEYWORDS=""
 IUSE=""
 
 DEPEND="
-	>=dev-qt/qtbase-${QTMIN}:6[dbus,gui,widgets]
+	>=dev-qt/qtbase-${QTMIN}:6[dbus,gui]
+	>=dev-qt/qtdeclarative:6
+	>=dev-qt/qtmultimedia-${QTMIN}:6
 	>=kde-frameworks/kconfig-${KFMIN}:6
-	>=kde-frameworks/kconfigwidgets-${KFMIN}:6
 	>=kde-frameworks/kcoreaddons-${KFMIN}:6
 	>=kde-frameworks/kcrash-${KFMIN}:6
-	>=kde-frameworks/kdbusaddons-${KFMIN}:6
 	>=kde-frameworks/ki18n-${KFMIN}:6
 	>=kde-frameworks/kio-${KFMIN}:6
-	>=kde-frameworks/kjobwidgets-${KFMIN}:6
-	>=kde-frameworks/kparts-${KFMIN}:6
+	>=kde-frameworks/kirigami-${KFMIN}:6
 	>=kde-frameworks/kservice-${KFMIN}:6
-	>=kde-frameworks/kwidgetsaddons-${KFMIN}:6
-	>=kde-frameworks/kwindowsystem-${KFMIN}:6
-	>=kde-frameworks/kxmlgui-${KFMIN}:6
-	>=kde-frameworks/solid-${KFMIN}:6
-	>=media-libs/phonon-4.12.0[qt6(+)]
 "
 RDEPEND="${DEPEND}"
 BDEPEND="sys-devel/gettext"

commit 2c3afa7f61798d51e750df627cb9c380fc4cc245

kde-apps/dragon: fix typo
Fixes: 2b20c2d1d871eabb72bdace5c21b5963fab7a85e
Signed-off-by: Sam James <sam@gentoo.org>

kde-apps/dragon/dragon-9999.ebuild
@@ -18,7 +18,7 @@ IUSE=""
 
 DEPEND="
 	>=dev-qt/qtbase-${QTMIN}:6[dbus,gui]
-	>=dev-qt/qtdeclarative:6
+	>=dev-qt/qtdeclarative-${QTMIN}:6
 	>=dev-qt/qtmultimedia-${QTMIN}:6
 	>=kde-frameworks/kconfig-${KFMIN}:6
 	>=kde-frameworks/kcoreaddons-${KFMIN}:6