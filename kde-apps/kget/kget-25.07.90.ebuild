gentoo/kde

commit 9613d23f5a653530661e091726861c9396e98695

kde-apps/kget: depend on split app-crypt/gpgme modules
Signed-off-by: Andreas Sturmlechner <asturm@gentoo.org>

kde-apps/kget/kget-9999.ebuild
@@ -39,7 +39,7 @@ RDEPEND="
 	>=kde-frameworks/kxmlgui-${KFMIN}:6
 	>=kde-frameworks/solid-${KFMIN}:6
 	bittorrent? ( net-libs/libktorrent:6 )
-	gpg? ( >=app-crypt/gpgme-1.23.1-r1:=[qt6(-)] )
+	gpg? ( dev-libs/qgpgme:= )
 	mms? ( media-libs/libmms )
 	sqlite? ( dev-db/sqlite:3 )
 "

kde-apps/kget/kget-25.04.3.ebuild
@@ -39,7 +39,7 @@ RDEPEND="
 	>=kde-frameworks/kxmlgui-${KFMIN}:6
 	>=kde-frameworks/solid-${KFMIN}:6
 	bittorrent? ( net-libs/libktorrent:6 )
-	gpg? ( >=app-crypt/gpgme-1.23.1-r1:=[qt6(-)] )
+	gpg? ( dev-libs/qgpgme:= )
 	mms? ( media-libs/libmms )
 	sqlite? ( dev-db/sqlite:3 )
 "

commit 3648fabe8e74a9cd6002e161425eb764d185d0e6

kde-apps/kget: also depend on gpgmepp for completeness
Its explicitly included as a dependency in the project alongside the qt
bindings package.

Fixes: 9613d23f5a653530661e091726861c9396e98695
Signed-off-by: Alfred Wingate <parona@protonmail.com>

kde-apps/kget/kget-25.07.90.ebuild
@@ -40,7 +40,10 @@ RDEPEND="
 	>=kde-frameworks/kxmlgui-${KFMIN}:6
 	>=kde-frameworks/solid-${KFMIN}:6
 	bittorrent? ( >=net-libs/libktorrent-${PVCUT}:6 )
-	gpg? ( dev-libs/qgpgme:= )
+	gpg? (
+		dev-cpp/gpgmepp:=
+		dev-libs/qgpgme:=
+	)
 	mms? ( media-libs/libmms )
 	sqlite? ( dev-db/sqlite:3 )
 "

kde-apps/kget/kget-25.08.0.ebuild
@@ -40,7 +40,10 @@ RDEPEND="
 	>=kde-frameworks/kxmlgui-${KFMIN}:6
 	>=kde-frameworks/solid-${KFMIN}:6
 	bittorrent? ( >=net-libs/libktorrent-${PVCUT}:6 )
-	gpg? ( dev-libs/qgpgme:= )
+	gpg? (
+		dev-cpp/gpgmepp:=
+		dev-libs/qgpgme:=
+	)
 	mms? ( media-libs/libmms )
 	sqlite? ( dev-db/sqlite:3 )
 "

kde-apps/kget/kget-25.08.49.9999.ebuild
@@ -40,7 +40,10 @@ RDEPEND="
 	>=kde-frameworks/kxmlgui-${KFMIN}:6
 	>=kde-frameworks/solid-${KFMIN}:6
 	bittorrent? ( >=net-libs/libktorrent-${PVCUT}:6 )
-	gpg? ( dev-libs/qgpgme:= )
+	gpg? (
+		dev-cpp/gpgmepp:=
+		dev-libs/qgpgme:=
+	)
 	mms? ( media-libs/libmms )
 	sqlite? ( dev-db/sqlite:3 )
 "

kde-apps/kget/kget-9999.ebuild
@@ -40,7 +40,10 @@ RDEPEND="
 	>=kde-frameworks/kxmlgui-${KFMIN}:6
 	>=kde-frameworks/solid-${KFMIN}:6
 	bittorrent? ( >=net-libs/libktorrent-${PVCUT}:6 )
-	gpg? ( dev-libs/qgpgme:= )
+	gpg? (
+		dev-cpp/gpgmepp:=
+		dev-libs/qgpgme:=
+	)
 	mms? ( media-libs/libmms )
 	sqlite? ( dev-db/sqlite:3 )
 "