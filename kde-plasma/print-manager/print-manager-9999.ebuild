gentoo/kde

commit 32526fcbb08a94a837b1eb6f3a826daa83bcb61b

kde-plasma/print-manager: add kde-frameworks/kitemmodels dependency
Upstream commit: ddc8d329d765f0d2bb3e4a1495b2263e28604e9e

Signed-off-by: Kostadin Shishmanov <kostadinshishmanov@protonmail.com>
Part-of: https://codeberg.org/gentoo/kde/pulls/3
Merges: https://codeberg.org/gentoo/kde/pulls/3
Signed-off-by: Sam James <sam@gentoo.org>

kde-plasma/print-manager/print-manager-9999.ebuild
@@ -32,10 +32,12 @@ DEPEND="
 	>=kde-frameworks/kwindowsystem-${KFMIN}:6
 	>=kde-plasma/libplasma-${KDE_CATV}:6=
 	>=net-print/cups-2.4
+	test? ( >=kde-frameworks/kitemmodels-${KFMIN}:6 )
 "
 RDEPEND="${DEPEND}
 	!<kde-plasma/print-manager-23.08.5-r100:5
 	>=kde-frameworks/kdeclarative-${KFMIN}:6
+	>=kde-frameworks/kitemmodels-${KFMIN}:6
 	gtk? ( app-admin/system-config-printer )
 "
 BDEPEND=">=kde-frameworks/kcmutils-${KFMIN}:6"

commit e9e60c62f61ebf7b0846dc0f388041a6c15815bc

kde-plasma/print-manager: fix test handling
Add missing test USE and also don't let it leak into RDEPEND.

Fixes: 32526fcbb08a94a837b1eb6f3a826daa83bcb61b
Signed-off-by: Sam James <sam@gentoo.org>

kde-plasma/print-manager/print-manager-9999.ebuild
@@ -12,7 +12,8 @@ DESCRIPTION="Manage CUPS print jobs and printers in Plasma"
 LICENSE="GPL-2" # TODO: CHECK
 SLOT="6"
 KEYWORDS=""
-IUSE="+gtk"
+IUSE="+gtk test"
+RESTRICT="!test? ( test )"
 
 DEPEND="
 	>=dev-qt/qt5compat-${QTMIN}:6
@@ -32,7 +33,6 @@ DEPEND="
 	>=kde-frameworks/kwindowsystem-${KFMIN}:6
 	>=kde-plasma/libplasma-${KDE_CATV}:6=
 	>=net-print/cups-2.4
-	test? ( >=kde-frameworks/kitemmodels-${KFMIN}:6 )
 "
 RDEPEND="${DEPEND}
 	!<kde-plasma/print-manager-23.08.5-r100:5
@@ -40,6 +40,7 @@ RDEPEND="${DEPEND}
 	>=kde-frameworks/kitemmodels-${KFMIN}:6
 	gtk? ( app-admin/system-config-printer )
 "
+DEPEND+=" test? ( >=kde-frameworks/kitemmodels-${KFMIN}:6 )"
 BDEPEND=">=kde-frameworks/kcmutils-${KFMIN}:6"
 
 src_configure() {