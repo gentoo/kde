gentoo/kde

commit 1c3e9395d00fe14659e5c984cb85683fbc5d8733

dev-libs/cxx-rust-cssparser: add 1.0.0
Bug: https://bugs.gentoo.org/974466
Signed-off-by: Alfred Wingate <parona@protonmail.com>
Part-of: https://codeberg.org/gentoo/kde/pulls/4
Signed-off-by: Sam James <sam@gentoo.org>

dev-libs/cxx-rust-cssparser/cxx-rust-cssparser-1.0.0.ebuild
@@ -1,0 +1,48 @@
+# Copyright 2026 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+CRATES="
+"
+RUST_MIN_VER="1.85.0"
+
+# TODO: ECMGenerateQDoc
+ECM_TEST="true"
+inherit cargo ecm kde.org toolchain-funcs
+
+DESCRIPTION="A C++ library for parsing CSS that uses the Rust cssparser crate internally"
+HOMEPAGE="https://invent.kde.org/libraries/cxx-rust-cssparser"
+
+if [[ ${KDE_BUILD_TYPE} == release ]]; then
+	SRC_URI="mirror://kde/stable/${KDE_ORG_NAME}/${KDE_ORG_NAME}-${PV}.tar.xz"
+	KEYWORDS="~amd64"
+	if [[ ${PKGBUMPING} != ${PVR} ]]; then
+		SRC_URI+=" https://github.com/gentoo-crate-dist/${PN}/releases/download/v${PV}/${P}-crates.tar.xz"
+	fi
+fi
+
+LICENSE="BSD-2 CC0-1.0 || ( LGPL-2.1 LGPL-3 )"
+# Dependent crate licenses
+LICENSE+=" Apache-2.0 MIT MPL-2.0 Unicode-3.0 ZLIB"
+SLOT="0"
+
+BDEPEND="dev-build/corrosion"
+
+src_unpack() {
+	if [[ ${KDE_BUILD_TYPE} == live ]]; then
+		git-r3_src_unpack
+		default
+		S="${S}/rust" cargo_live_src_unpack
+	else
+		cargo_src_unpack
+	fi
+}
+
+src_configure() {
+	# Rust extensions are incompatible with C/C++ LTO compiler see e.g.
+	# https://bugs.gentoo.org/910220
+	filter-lto
+
+	ecm_src_configure
+}

dev-libs/cxx-rust-cssparser/Manifest
@@ -1,0 +1,2 @@
+DIST cxx-rust-cssparser-1.0.0-crates.tar.xz 3701580 BLAKE2B 8f105353145e2ada60efbaff22e017c2ac37b1872311c387b83de0af3071af2c4cd3908a33c46792000a600f577e5cf94d6de102285bf39e52f1f917ccda8d60 SHA512 d3014606e8f02bc159af3505dc3ed78882715edab8ffd2d859bfe23599041f23690ccc5bbf8b74ce6aa26ff53ec080d879b099e1fa0b9fec680d590a3b0ebef8
+DIST cxx-rust-cssparser-1.0.0.tar.xz 55448 BLAKE2B f22e897bd26a2b0565eea4dd1b7a99b742c0be3dcd97bb3ad46f00e05f00970b9381bd50c624af20a471374f0804e44dd326818f5391b722be8a0fdf053dbf6b SHA512 6fb0f7deb6e3bfcfdc120677e4a9c83f6923531ef92cb97175973c730f3fbc22a168330e788759a2605caa386d061d16771a7ed7080a2539d4ccf67a9b4e83fe

commit d189ef885f45d374078fa299e1e08a2fc5e9ca92

dev-libs/cxx-rust-cssparser: use the the correct eclass for filter-lto
Fixes: 1c3e9395d00fe14659e5c984cb85683fbc5d8733
Fixes: 95b52641b5c08271875ce348efe882e9a4bad326
Signed-off-by: Alfred Wingate <parona@protonmail.com>
Part-of: https://codeberg.org/gentoo/kde/pulls/5
Signed-off-by: Sam James <sam@gentoo.org>

dev-libs/cxx-rust-cssparser/cxx-rust-cssparser-1.0.0.ebuild
@@ -9,7 +9,7 @@ RUST_MIN_VER="1.85.0"
 
 # TODO: ECMGenerateQDoc
 ECM_TEST="true"
-inherit cargo ecm kde.org toolchain-funcs
+inherit cargo ecm flag-o-matic kde.org
 
 DESCRIPTION="C++ library for parsing CSS that uses the Rust cssparser crate internally"
 HOMEPAGE="https://invent.kde.org/libraries/cxx-rust-cssparser"

dev-libs/cxx-rust-cssparser/cxx-rust-cssparser-9999.ebuild
@@ -9,7 +9,7 @@ RUST_MIN_VER="1.85.0"
 
 # TODO: ECMGenerateQDoc
 ECM_TEST="true"
-inherit cargo ecm kde.org toolchain-funcs
+inherit cargo ecm flag-o-matic kde.org
 
 DESCRIPTION="C++ library for parsing CSS that uses the Rust cssparser crate internally"
 HOMEPAGE="https://invent.kde.org/libraries/cxx-rust-cssparser"