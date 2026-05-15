gentoo/kde

commit c19b5da2b8ec356a8060619a4fc43b535b30b725

kde-plasma/kdeplasma-addons: handle corrosion
Upstream commit: 02d853e74c61dffd60d048050c8c09bd09e52a33

Signed-off-by: Alfred Wingate <parona@protonmail.com>
Part-of: https://codeberg.org/gentoo/kde/pulls/4
Merges: https://codeberg.org/gentoo/kde/pulls/4
Signed-off-by: Sam James <sam@gentoo.org>

kde-plasma/kdeplasma-addons/kdeplasma-addons-9999.ebuild
@@ -3,14 +3,24 @@
 
 EAPI=8
 
+CRATES="
+"
+RUST_MIN_VER="1.87.0"
+
 ECM_HANDBOOK="forceoptional"
 KFMIN=9999
 QTMIN=6.10.1
-inherit ecm plasma.kde.org optfeature xdg
+inherit cargo ecm plasma.kde.org optfeature toolchain-funcs xdg
 
 DESCRIPTION="Extra Plasma applets and engines"
 
+if [[ ${KDE_BUILD_TYPE} == release ]] && [[ ${PKGBUMPING} != ${PVR} ]]; then
+	SRC_URI+=" https://github.com/gentoo-crate-dist/${PN}/releases/download/v${PV}/${P}-crates.tar.xz"
+fi
+
 LICENSE="GPL-2 LGPL-2"
+# Dependent crate licenses
+LICENSE+=" GPL-3 MIT Unicode-3.0 ZLIB"
 SLOT="6"
 KEYWORDS=""
 IUSE="+alternate-calendar share webengine"
@@ -55,8 +65,23 @@ RDEPEND="${DEPEND}
 	>=kde-frameworks/kirigami-${KFMIN}:6
 	>=kde-frameworks/kitemmodels-${KFMIN}:6
 "
+BDEPEND="dev-build/corrosion"
+
+src_unpack() {
+	if [[ ${KDE_BUILD_TYPE} == live ]]; then
+		git-r3_src_unpack
+		default
+		S="${S}/kdeds/kameleon/qmk/kameleon-qmk-helper" cargo_live_src_unpack
+	else
+		cargo_src_unpack
+	fi
+}
 
 src_configure() {
+	# Rust extensions are incompatible with C/C++ LTO compiler see e.g.
+	# https://bugs.gentoo.org/910220
+	filter-lto
+
 	local mycmakeargs=(
 		$(cmake_use_find_package alternate-calendar ICU)
 		$(cmake_use_find_package share KF6Purpose)

commit f080cce820b31db45809496c298251b9a756951f

kde-plasma/kdeplasma-addons: use the the correct eclass for filter-lto
Fixes: c19b5da2b8ec356a8060619a4fc43b535b30b725
Signed-off-by: Alfred Wingate <parona@protonmail.com>
Part-of: https://codeberg.org/gentoo/kde/pulls/5
Merges: https://codeberg.org/gentoo/kde/pulls/5
Signed-off-by: Sam James <sam@gentoo.org>

kde-plasma/kdeplasma-addons/kdeplasma-addons-9999.ebuild
@@ -10,7 +10,7 @@ RUST_MIN_VER="1.87.0"
 ECM_HANDBOOK="forceoptional"
 KFMIN=9999
 QTMIN=6.10.1
-inherit cargo ecm plasma.kde.org optfeature toolchain-funcs xdg
+inherit cargo ecm flag-o-matic plasma.kde.org optfeature xdg
 
 DESCRIPTION="Extra Plasma applets and engines"
 