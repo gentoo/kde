gentoo/kde

commit 7c03da1d54edcce4fe4bdae4ee74afc707792f67

dev-libs/cxx-rust-cssparser: new dependency to avoid corrosion-6 breakage
Signed-off-by: Alfred Wingate <parona@protonmail.com>
Part-of: https://codeberg.org/gentoo/kde/pulls/5
Signed-off-by: Sam James <sam@gentoo.org>

dev-libs/cxx-rust-cssparser/cxx-rust-cssparser-1.0.0.ebuild
@@ -27,7 +27,10 @@ LICENSE="BSD-2 CC0-1.0 || ( LGPL-2.1 LGPL-3 )"
 LICENSE+=" Apache-2.0 MIT MPL-2.0 Unicode-3.0 ZLIB"
 SLOT="0"
 
-BDEPEND="dev-build/corrosion"
+BDEPEND="
+	dev-build/corrosion
+	dev-build/cxxbridge-cmd
+"
 
 src_configure() {
 	# Rust extensions are incompatible with C/C++ LTO compiler see e.g.

dev-libs/cxx-rust-cssparser/cxx-rust-cssparser-9999.ebuild
@@ -27,7 +27,10 @@ LICENSE="BSD-2 CC0-1.0 || ( LGPL-2.1 LGPL-3 )"
 LICENSE+=" Apache-2.0 MIT MPL-2.0 Unicode-3.0 ZLIB"
 SLOT="0"
 
-BDEPEND="dev-build/corrosion"
+BDEPEND="
+	dev-build/corrosion
+	dev-build/cxxbridge-cmd
+"
 
 src_configure() {
 	# Rust extensions are incompatible with C/C++ LTO compiler see e.g.

commit f189bde7bab9775b23c8526bc9363775259f1125

dev-libs/cxx-rust-cssparser: require the exact version of cxxbridge-cmd
* Not great, but its a workaround until corrosion-6 is modified or
  something else is figured out.

Bug: https://bugs.gentoo.org/975121
Fixes: 7c03da1d54edcce4fe4bdae4ee74afc707792f67
Signed-off-by: Alfred Wingate <parona@protonmail.com>
Part-of: https://codeberg.org/gentoo/kde/pulls/6
Signed-off-by: Sam James <sam@gentoo.org>

dev-libs/cxx-rust-cssparser/cxx-rust-cssparser-1.0.0.ebuild
@@ -27,9 +27,10 @@ LICENSE="BSD-2 CC0-1.0 || ( LGPL-2.1 LGPL-3 )"
 LICENSE+=" Apache-2.0 MIT MPL-2.0 Unicode-3.0 ZLIB"
 SLOT="0"
 
+# Short term solution bug #975121
 BDEPEND="
 	dev-build/corrosion
-	dev-build/cxxbridge-cmd
+	~dev-build/cxxbridge-cmd-1.0.194
 "
 
 src_configure() {

dev-libs/cxx-rust-cssparser/cxx-rust-cssparser-9999.ebuild
@@ -27,9 +27,10 @@ LICENSE="BSD-2 CC0-1.0 || ( LGPL-2.1 LGPL-3 )"
 LICENSE+=" Apache-2.0 MIT MPL-2.0 Unicode-3.0 ZLIB"
 SLOT="0"
 
+# Short term solution bug #975121
 BDEPEND="
 	dev-build/corrosion
-	dev-build/cxxbridge-cmd
+	~dev-build/cxxbridge-cmd-1.0.194
 "
 
 src_configure() {