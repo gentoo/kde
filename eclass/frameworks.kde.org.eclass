gentoo/kde

commit f7e85b20d1192e12dbb812f40ef83a54627b3057

frameworks.kde.org.eclass: add verify-sig support
Signed-off-by: Alfred Wingate <parona@protonmail.com>
Part-of: https://codeberg.org/gentoo/kde/pulls/10
Signed-off-by: Sam James <sam@gentoo.org>

eclass/frameworks.kde.org.eclass
@@ -38,6 +38,15 @@ readonly KDE_CATV
 # For proper description see kde.org.eclass manpage.
 KDE_PV_UNRELEASED=( )
 
+# Set _KDE_RELEASE_MANAGER to enable signature checking.
+case ${PV} in
+	*_p*) ;;
+	*) _KDE_RELEASE_MANAGER="nicolasfella" ;;
+esac
+# Allow overriding in ebuild if necessary
+: ${KDE_RELEASE_MANAGER:=${_KDE_RELEASE_MANAGER}}
+unset _KDE_RELEASE_MANAGER
+
 inherit kde.org
 
 HOMEPAGE="https://develop.kde.org/products/frameworks/"
@@ -66,6 +75,10 @@ _KDE_SRC_URI="mirror://kde/stable/frameworks/${KDE_CATV}/"
 
 if [[ ${KDE_BUILD_TYPE} != live && -z ${KDE_ORG_COMMIT} ]]; then
 	SRC_URI="${_KDE_SRC_URI}${KDE_ORG_TAR_PN}-${PV}.tar.xz"
+
+	if [[ -n ${KDE_RELEASE_MANAGER} ]]; then
+		SRC_URI+=" verify-sig? ( ${_KDE_SRC_URI}${KDE_ORG_TAR_PN}-${PV}.tar.xz.sig )"
+	fi
 fi
 
 fi

commit d28f3fbd21a8c638be5c42e149d0b22086ca89bb

eclass: document KDE_RELEASE_MANAGER in each eclass
Fixes: f7e85b20d1192e12dbb812f40ef83a54627b3057
Fixes: b7762a6341ef972f5f4d08e56cfa3d6efd1c3aba
Fixes: 585e2b0aca3364a822f8abf10f4cf87dbdc1b483
Signed-off-by: Alfred Wingate <parona@protonmail.com>
Part-of: https://codeberg.org/gentoo/kde/pulls/17
Signed-off-by: Sam James <sam@gentoo.org>

eclass/frameworks.kde.org.eclass
@@ -46,7 +46,11 @@ case ${PV} in
 	6.26.*) ;;
 	*) _KDE_RELEASE_MANAGER="nicolasfella" ;;
 esac
-# Allow overriding in ebuild if necessary
+
+# @ECLASS_VARIABLE: KDE_RELEASE_MANAGER
+# @PRE_INHERIT
+# @DESCRIPTION:
+# Identity of a release manager used to verify distfile signature.
 : ${KDE_RELEASE_MANAGER:=${_KDE_RELEASE_MANAGER}}
 unset _KDE_RELEASE_MANAGER
 

eclass/gear.kde.org.eclass
@@ -39,7 +39,11 @@ case ${PV} in
 	26.04.*) ;;
 	*) _KDE_RELEASE_MANAGER="heikobecker" ;;
 esac
-# Allow overriding in ebuild if necessary
+
+# @ECLASS_VARIABLE: KDE_RELEASE_MANAGER
+# @PRE_INHERIT
+# @DESCRIPTION:
+# Identity of a release manager used to verify distfile signature.
 : ${KDE_RELEASE_MANAGER:=${_KDE_RELEASE_MANAGER}}
 unset _KDE_RELEASE_MANAGER
 

eclass/plasma.kde.org.eclass
@@ -45,7 +45,11 @@ case ${PV} in
 	6.6.*) ;;
 	*) _KDE_RELEASE_MANAGER="bshah" ;;
 esac
-# Allow overriding in ebuild if necessary
+
+# @ECLASS_VARIABLE: KDE_RELEASE_MANAGER
+# @PRE_INHERIT
+# @DESCRIPTION:
+# Identity of a release manager used to verify distfile signature.
 : ${KDE_RELEASE_MANAGER:=${_KDE_RELEASE_MANAGER}}
 unset _KDE_RELEASE_MANAGER
 