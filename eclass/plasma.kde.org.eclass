gentoo/kde

commit 0d7f5e22f0823a4b007a6b625ea039649b23dbec

eclass: avoid retroactively enabling verify-sig for old releases
Make life easier for people using eclass-overrides.

Signed-off-by: Alfred Wingate <parona@protonmail.com>
Part-of: https://codeberg.org/gentoo/kde/pulls/16
Merges: https://codeberg.org/gentoo/kde/pulls/16
Signed-off-by: Sam James <sam@gentoo.org>

eclass/plasma.kde.org.eclass
@@ -41,6 +41,8 @@ KDE_PV_UNRELEASED=( )
 # Set _KDE_RELEASE_MANAGER to enable signature checking.
 case ${PV} in
 	*_p*) ;;
+	# avoid retroactively enabling verify-sig for eclass-overrides users
+	6.6.*) ;;
 	*) _KDE_RELEASE_MANAGER="bshah" ;;
 esac
 # Allow overriding in ebuild if necessary

eclass/frameworks.kde.org.eclass
@@ -41,6 +41,9 @@ KDE_PV_UNRELEASED=( )
 # Set _KDE_RELEASE_MANAGER to enable signature checking.
 case ${PV} in
 	*_p*) ;;
+	# avoid retroactively enabling verify-sig for eclass-overrides users
+	6.25.*) ;;
+	6.26.*) ;;
 	*) _KDE_RELEASE_MANAGER="nicolasfella" ;;
 esac
 # Allow overriding in ebuild if necessary

eclass/gear.kde.org.eclass
@@ -34,6 +34,9 @@ KDE_PV_UNRELEASED=( )
 # Set _KDE_RELEASE_MANAGER to enable signature checking.
 case ${PV} in
 	*_p*) ;;
+	# avoid retroactively enabling verify-sig for eclass-overrides users
+	25.12.*) ;;
+	26.04.*) ;;
 	*) _KDE_RELEASE_MANAGER="heikobecker" ;;
 esac
 # Allow overriding in ebuild if necessary

commit 6a71f3793596d7e28115fc67a9aadd74d9e62afd

plasma.kde.org.eclass: specify versions to keep signatures for 6.7 beta
Fixes: 0d7f5e22f0823a4b007a6b625ea039649b23dbec
Signed-off-by: Alfred Wingate <parona@protonmail.com>
Part-of: https://codeberg.org/gentoo/kde/pulls/17
Merges: https://codeberg.org/gentoo/kde/pulls/17
Signed-off-by: Sam James <sam@gentoo.org>

eclass/plasma.kde.org.eclass
@@ -42,7 +42,7 @@ KDE_PV_UNRELEASED=( )
 case ${PV} in
 	*_p*) ;;
 	# avoid retroactively enabling verify-sig for eclass-overrides users
-	6.6.*) ;;
+	6.6.4|6.6.5) ;;
 	*) _KDE_RELEASE_MANAGER="bshah" ;;
 esac
 