gentoo/kde

commit 8c3c4ef406f5020ac50d89c95121f79826e7c829

kde-plasma/union: wire up CSS via cxx-rust-cssparser
CSS is the default input format per README, and I don't think it's expected
to have this off really, so make it mandatory.

Signed-off-by: Sam James <sam@gentoo.org>

kde-plasma/union/union-9999.ebuild
@@ -40,14 +40,17 @@ DEPEND="
 RDEPEND="${DEPEND}
 	widgets? ( kde-plasma/breeze:6 )
 "
-BDEPEND=">=dev-qt/qttools-${QTMIN}:6[qdoc]"
+BDEPEND="
+	dev-libs/cxx-rust-cssparser
+	>=dev-qt/qttools-${QTMIN}:6[qdoc]
+"
 
 DOCS=( README.md )
 
 src_configure() {
 	local mycmakeargs=(
 		-DBUILD_INPUT_PLASMASVG=OFF # defunct as of 2025-10-09
-		-DBUILD_INPUT_CSS=OFF # cxx-rust-cssparser not packaged
+		-DBUILD_INPUT_CSS=ON # CSS is the default input format
 		-DBUILD_WITH_KDEFRAMEWORKS=ON # required by BUILD_PLATFORM_PLASMA
 		-DBUILD_PLATFORM_PLASMA=ON
 		-DBUILD_OUTPUT_KIRIGAMI=ON

kde-plasma/union/union-6.7.49.9999.ebuild
@@ -38,6 +38,7 @@ DEPEND="
 	>=kde-frameworks/kirigami-${KFMIN}:6
 "
 RDEPEND="${DEPEND}
+	dev-libs/cxx-rust-cssparser
 	widgets? ( kde-plasma/breeze:6 )
 "
 BDEPEND=">=dev-qt/qttools-${QTMIN}:6[qdoc]"
@@ -47,7 +48,7 @@ DOCS=( README.md )
 src_configure() {
 	local mycmakeargs=(
 		-DBUILD_INPUT_PLASMASVG=OFF # defunct as of 2025-10-09
-		-DBUILD_INPUT_CSS=OFF # cxx-rust-cssparser not packaged
+		-DBUILD_INPUT_CSS=ON # CSS is the default input format
 		-DBUILD_WITH_KDEFRAMEWORKS=ON # required by BUILD_PLATFORM_PLASMA
 		-DBUILD_PLATFORM_PLASMA=ON
 		-DBUILD_OUTPUT_KIRIGAMI=ON

commit fd1bb109a79a2feda47b2753682f94e8691d5e6c

kde-plasma/union: fix dev-libs/cxx-rust-cssparser dep
In 8c3c4ef406f5020ac50d89c95121f79826e7c829, I had it in RDEPEND-only
in one ebuild, and BDEPEND-only in another. It should be in DEPEND+RDEPEND.

Fixes: 8c3c4ef406f5020ac50d89c95121f79826e7c829
Signed-off-by: Sam James <sam@gentoo.org>

kde-plasma/union/union-6.7.90.ebuild
@@ -27,6 +27,7 @@ IUSE="tools +widgets"
 # 		kde-plasma/libplasma:6=
 # 	)
 DEPEND="
+	>=dev-libs/cxx-rust-cssparser-1.1.0
 	>=dev-qt/qtbase-${QTMIN}:6[dbus,gui,widgets?]
 	>=dev-qt/qtdeclarative-${QTMIN}:6
 	>=dev-qt/qtshadertools-${QTMIN}:6
@@ -42,7 +43,6 @@ RDEPEND="${DEPEND}
 	widgets? ( kde-plasma/breeze:6 )
 "
 BDEPEND="
-	>=dev-libs/cxx-rust-cssparser-1.1.0
 	>=dev-qt/qttools-${QTMIN}:6[qdoc]
 "
 

kde-plasma/union/union-6.8.49.9999.ebuild
@@ -27,6 +27,7 @@ IUSE="tools +widgets"
 # 		kde-plasma/libplasma:6=
 # 	)
 DEPEND="
+	>=dev-libs/cxx-rust-cssparser-1.1.0
 	>=dev-qt/qtbase-${QTMIN}:6[dbus,gui,widgets?]
 	>=dev-qt/qtdeclarative-${QTMIN}:6
 	>=dev-qt/qtshadertools-${QTMIN}:6
@@ -42,7 +43,6 @@ RDEPEND="${DEPEND}
 	widgets? ( kde-plasma/breeze:6 )
 "
 BDEPEND="
-	>=dev-libs/cxx-rust-cssparser-1.1.0
 	>=dev-qt/qttools-${QTMIN}:6[qdoc]
 "
 

kde-plasma/union/union-9999.ebuild
@@ -27,6 +27,7 @@ IUSE="tools +widgets"
 # 		kde-plasma/libplasma:6=
 # 	)
 DEPEND="
+	>=dev-libs/cxx-rust-cssparser-1.1.0
 	>=dev-qt/qtbase-${QTMIN}:6[dbus,gui,widgets?]
 	>=dev-qt/qtdeclarative-${QTMIN}:6
 	>=dev-qt/qtshadertools-${QTMIN}:6
@@ -42,7 +43,6 @@ RDEPEND="${DEPEND}
 	widgets? ( kde-plasma/breeze:6 )
 "
 BDEPEND="
-	>=dev-libs/cxx-rust-cssparser-1.1.0
 	>=dev-qt/qttools-${QTMIN}:6[qdoc]
 "
 